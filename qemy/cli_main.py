1b16095d58eaf84d72cecb043c530f3604cc096d
ryan-rashidian <ryanrashidian@gmail.com>
2025-06-05 11:23:39 -0700

    ENH Added and cleaned up help commands for CLI in cli_main.py

--- qemy/cli_main.py
+++ qemy/cli_main.py
@@ -36,189 +36,189 @@ class QemyShell(cmd.Cmd):
         except Exception as e:
             print(f"Failed to load session ERROR:\n{e}")
     def help_session(self):
-        print('Starts a session.')
-        print('Usage: session')
+        print("Starts a session.")
+        print("Usage: session")
 #=============================================================================#
 ################################### SEC #######################################
 #=============================================================================#
     def do_dcf(self, arg):
         cli_edgar.dcf(arg=arg)
     def help_dcf(self):
-        print('Performs DCF model evaluation on given ticker.')
-        print('Usage: dcf <TICKER>')
+        print("Performs DCF model evaluation on given ticker.")
+        print("Usage: dcf <TICKER>")
 #=============================================================================#
     def do_filing(self, arg):
         df_return = cli_edgar.filing(arg=arg, ticker_df=self.ticker_df)
         if isinstance(df_return, pd.DataFrame):
             self.ticker_df = df_return
     def help_filing(self):
-        print('Fetches latest 10K/10Q metrics for given ticker.')
-        print('Usage: filing <TICKER>\nFlags:')
-        print('(-r --request) --- Fetches filing data directly from SEC EDGAR API.')
+        print("Fetches latest 10K/10Q metrics for given ticker.")
+        print("Usage: filing <TICKER>\nFlags:")
+        print("(-r --request) --- Fetches filing data directly from SEC EDGAR API.")
 #=============================================================================#
     def do_bulk_refresh(self, _):
         cli_edgar.bulk_refresh()
     def help_bulk_refresh(self):
-        print('Re-downloads bulk data from SEC EDGAR API with latest filings.')
-        print('Note: every bulk_refresh will download and unzip ~20GB of filing data.')
-        print('All previous bulk data will be overwritten.')
+        print("Re-downloads bulk data from SEC EDGAR API with latest filings.")
+        print("Note: every bulk_refresh will download and unzip ~20GB of filing data.")
+        print("All previous bulk data will be overwritten.")
 #=============================================================================#
 ################################## FRED #######################################
 #=============================================================================#
     def do_rfr(self, _):
         cli_fred.rfr()
     def help_rfr(self):
-        print('Fetches latest 1 year T-Bill yield.')
-        print('Usage: rfr')
+        print("Fetches latest 1 year T-Bill yield.")
+        print("Usage: rfr")
 #=============================================================================#
     def do_cpi(self, arg):
         cli_fred.cpi(arg=arg)
     def help_cpi(self):
-        print('Fetches monthly CPI data.')
-        print('Usage: cpi -p <PERIOD>')
+        print("Fetches monthly CPI data.")
+        print("Usage: cpi -p <PERIOD>")
 #=============================================================================#
     def do_gdp(self, arg):
         cli_fred.gdp(arg=arg)
     def help_gdp(self):
-        print('Fetches quarterly GDP data.')
-        print('Usage: gdp -p <PERIOD>')
+        print("Fetches quarterly GDP data.")
+        print("Usage: gdp -p <PERIOD>")
 #=============================================================================#
     def do_sent(self, arg):
         cli_fred.sent(arg=arg)
     def help_sent(self):
-        print('Fetches cosumer sentiment data.')
-        print('Usage: sent -p <PERIOD>')
+        print("Fetches Cosumer Sentiment index data.")
+        print("Usage: sent -p <PERIOD>")
 #=============================================================================#
     def do_nfp(self, arg):
         cli_fred.nfp(arg=arg)
     def help_nfp(self):
-        print('Fetches non-farm payroll data.')
-        print('Usage: nfp -p <PERIOD>')
+        print("Fetches Nonfarm Payroll data.")
+        print("Usage: nfp -p <PERIOD>")
 #=============================================================================#
     def do_interest(self, arg):
         cli_fred.interest(arg=arg)
     def help_interest(self):
-        print('Fetches interest rate data.')
-        print('Usage: interest -p <PERIOD>')
+        print("Fetches Interest RED rate data.")
+        print("Usage: Interest -p <PERIOD>")
 #=============================================================================#
     def do_jobc(self, arg):
         cli_fred.jobc(arg=arg)
     def help_jobc(self):
-        print('Fetches jobless claim data.')
-        print('Usage: jobc -p <PERIOD>')
+        print("Fetches Jobless Claim data.")
+        print("Usage: jobc -p <PERIOD>")
 #=============================================================================#
     def do_unem(self, arg):
         cli_fred.unem(arg=arg)
     def help_unem(self):
-        print('Fetches unemployment data.')
-        print('Usage: unem -p <PERIOD>')
+        print("Fetches Unemployment data.")
+        print("Usage: unem -p <PERIOD>")
 #=============================================================================#
     def do_indp(self, arg):
         cli_fred.indp(arg=arg)
     def help_indp(self):
-        print('Fetches industrial production data.')
-        print('Usage: indp -p <PERIOD>')
+        print("Fetches Industrial Production data.")
+        print("Usage: indp -p <PERIOD>")
 #=============================================================================#
     def do_netex(self, arg):
         cli_fred.netex(arg=arg)
     def help_netex(self):
-        print('Fetches real net exports of goods and services data.')
-        print('Usage: netex -p <PERIOD>')
+        print("Fetches Real Net Exports of Goods and Services data.")
+        print("Usage: netex -p <PERIOD>")
 #=============================================================================#
 ################################## PRICE ######################################
 #=============================================================================#
     def do_price(self, arg):
         cli_tiingo.price(arg=arg)
     def help_price(self):
-        print('Fetches daily closing prices for given ticker.')
-        print('Usage: price <TICKER> -p <PERIOD>')
+        print("Fetches daily closing prices for given ticker.")
+        print("Usage: price <TICKER> -p <PERIOD>")
 #=============================================================================#
     def do_lr(self, arg):
         cli_tiingo.lr(arg=arg)
     def help_lr(self):
-        print('Fetches daily closing prices for given ticker and performs linear calculation.')
-        print('Usage: lr <TICKER> -p <PERIOD>')
-        print('Uses SPY ETF Trust as dependent variable.')
+        print("Fetches daily closing prices for given ticker and performs linear calculation.")
+        print("Usage: lr <TICKER> -p <PERIOD>")
+        print("Uses SPY ETF Trust as dependent variable.")
 #=============================================================================#
     def do_mcarlo(self, arg):
         cli_tiingo.monte_carlo(arg=arg)
     def help_mcarlo(self):
-        print('place holder')
-        print('Usage: mcarlo AAPL -p 2Y -n 1000')
+        print("Performs Monte Carlo simulations on given ticker's price history")
+        print("Usage: mcarlo AAPL -p 2Y -n 1000")
 #=============================================================================#
 ################################## PLOT #######################################
 #=============================================================================#
     def do_plot_price(self, arg):
         cli_plot.plot_price(arg=arg)
     def help_plot_price(self):
-        print('Fetches plot chart of log scaled daily prices for given ticker.')
-        print('Usage: plot_price <TICKER> -p <PERIOD>')
+        print("Fetches plot chart of log scaled daily prices for given ticker.")
+        print("Usage: plot_price <TICKER> -p <PERIOD>")
 #=============================================================================#
     def do_plot_lr(self, arg):
         cli_plot.plot_lr(arg=arg)
     def help_plot_lr(self):
-        print('Fetches linear regression beta comparison for given ticker.')
-        print('Usage: plot_lr <TICKER> -p <PERIOD>')
+        print("Fetches linear regression plot chart for given ticker and SPY.")
+        print("Usage: plot_lr <TICKER> -p <PERIOD>")
 #=============================================================================#
     def do_plot_mcarlo(self, arg):
         cli_plot.plot_monte_carlo(arg=arg)
     def help_plot_mcarlo(self):
-        print('test')
-        print('Usage: plot_mcarlo AAPL -p 2Y -n 1000')
+        print("Performs and plots Monte Carlo simulations on given ticker's price history")
+        print("Usage: plot_mcarlo AAPL -p 2Y -n 1000")
 #=============================================================================#
     def do_plot_cpi(self, arg):
         cli_plot.plot_cpi(arg=arg)
     def help_plot_cpi(self):
-        print('Fetches plot chart for CPI inflation % change from 1-Year ago.')
-        print('Usage: plot_cpi -p <PERIOD>')
+        print("Fetches plot chart for CPI inflation.")
+        print("Usage: plot_cpi -p <PERIOD>")
 #=============================================================================#
     def do_plot_gdp(self, arg):
         cli_plot.plot_gdp(arg=arg)
     def help_plot_gdp(self):
-        print('Fetches plot chart for GDP % change from 1-Year ago.')
-        print('Usage: plot_gdp -p <PERIOD>')
+        print("Fetches plot chart for GDP.")
+        print("Usage: plot_gdp -p <PERIOD>")
 #=============================================================================#
     def do_plot_sent(self, arg):
         cli_plot.plot_sent(arg=arg)
     def help_plot_sent(self):
-        print('Fetches plot chart for Sentiment % change.')
-        print('Usage: plot_sent -p <PERIOD>')
+        print("Fetches plot chart for Sentiment index.")
+        print("Usage: plot_sent -p <PERIOD>")
 #=============================================================================#
     def do_plot_nfp(self, arg):
         cli_plot.plot_nfp(arg=arg)
     def help_plot_nfp(self):
-        print('Fetches plot chart for Non-Farm Payroll % change from 1-Year ago.')
-        print('Usage: plot_nfp -p <PERIOD>')
+        print("Fetches plot chart for Non-Farm Payroll.")
+        print("Usage: plot_nfp -p <PERIOD>")
 #=============================================================================#
     def do_plot_interest(self, arg):
         cli_plot.plot_interest(arg=arg)
     def help_plot_interest(self):
-        print('Fetches plot chart for Interest rates, % change from 1-Year ago.')
-        print('Usage: plot_interest -p <PERIOD>')
+        print("Fetches plot chart for Interest FED rate.")
+        print("Usage: plot_interest -p <PERIOD>")
 #=============================================================================#
     def do_plot_jobc(self, arg):
         cli_plot.plot_jobc(arg=arg)
     def help_plot_jobc(self):
-        print('Fetches plot chart for Jobless Claims % change from 1-Year ago.')
-        print('Usage: plot_jobc -p <PERIOD>')
+        print("Fetches plot chart for Jobless Claims.")
+        print("Usage: plot_jobc -p <PERIOD>")
 #=============================================================================#
     def do_plot_unem(self, arg):
         cli_plot.plot_unem(arg=arg)
     def help_plot_unem(self):
-        print('Fetches plot chart for Unemployment rate, % change from 1-Year ago.')
-        print('Usage: plot_unem -p <PERIOD>')
+        print("Fetches plot chart for Unemployment rate.")
+        print("Usage: plot_unem -p <PERIOD>")
 #=============================================================================#
     def do_plot_indp(self, arg):
         cli_plot.plot_indp(arg=arg)
     def help_plot_indp(self):
-        print('Fetches plot chart for Industrial Production % change from 1-Year ago.')
-        print('Usage: plot_indp -p <PERIOD>')
+        print("Fetches plot chart for Industrial Production.")
+        print("Usage: plot_indp -p <PERIOD>")
 #=============================================================================#
     def do_plot_netex(self, arg):
         cli_plot.plot_netex(arg=arg)
     def help_plot_netex(self):
-        print('Fetches plot chart for Real Net Exports of Goods and Services.')
-        print('Usage: plot_netex -p <PERIOD>')
+        print("Fetches plot chart for Real Net Exports of Goods and Services.")
+        print("Usage: plot_netex -p <PERIOD>")
 #=============================================================================#
 ################################## CLI ########################################
 #=============================================================================#
@@ -226,8 +226,8 @@ class QemyShell(cmd.Cmd):
         print('Watchlist:')
         print(self.ticker_list)
     def help_watchlist(self):
-        print('Fetches current watchlist')
-        print('Usage: watchlist')
+        print("Fetches current watchlist")
+        print("Usage: watchlist")
 #=============================================================================#
     def do_table(self, arg):
         save_state = parse_arg.parse_arg_s(arg=arg, name='table')
@@ -235,7 +235,7 @@ class QemyShell(cmd.Cmd):
             try:
                 if save_state in ('Y', 'YES'):
                     save_to_csv(df=self.ticker_df)
-                    print('File saved...\n /qemy/exports/tables/')
+                    print("File saved...\n /qemy/exports/tables/")
                 else:
                     print('Filings:')
                     print(self.ticker_df.to_string(justify='left', formatters={
@@ -245,10 +245,10 @@ class QemyShell(cmd.Cmd):
             except Exception as e:
                 print(f"Could not fetch/save table. ERROR:\n{e}")
         else:
-            print('For valid syntax, Try: table -s yes')
+            print("For valid syntax, Try: table -s yes")
     def help_table(self):
-        print('Fetches table of current filings')
-        print('Usage: table')
+        print("Fetches table of current filings")
+        print("Usage: table")
 #=============================================================================#
 ################################ CLI UTIL #####################################
 #=============================================================================#
