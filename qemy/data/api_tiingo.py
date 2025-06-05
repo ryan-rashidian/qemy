9373244cf7d6dc6d5273fdaf8d6ea2f11f87ff12
ryan-rashidian <ryanrashidian@gmail.com>
2025-06-05 12:14:11 -0700

    ENH Added get_tiingo_quote function to api_tiingo.py module

--- qemy/data/api_tiingo.py
+++ qemy/data/api_tiingo.py
@@ -15,3 +15,12 @@ def get_tiingo_prices(ticker, period='1W', resample='daily', columns='close'):
     }
     return safe_status_get(url=url, headers=TIINGO_HEADERS, params=params)
 
+def get_tiingo_quote(tickers):
+    if isinstance(tickers, str):
+        tickers = [tickers]
+    url = f"https://api.tiingo.com/iex/{','.join(tickers)}"
+    response = safe_status_get(url=url, headers=TIINGO_HEADERS)
+    if not isinstance(response, list):
+        return {}
+    return {entry["ticker"]: entry for entry in response if "ticker" in entry}
+
