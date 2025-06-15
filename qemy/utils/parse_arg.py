c1abb3154b26d7d88e7f75043d25aae497d53bcd
ryan-rashidian <ryanrashidian@gmail.com>
2025-06-14 20:30:14 -0700

    ENH Added -h --help flag to parse_args function and removed arg requirement for -t --ticker flag

--- qemy/utils/parse_arg.py
+++ qemy/utils/parse_arg.py
@@ -2,20 +2,21 @@ import shlex
 import argparse
 
 ARGUMENTS = {
-    'save':          lambda p: p.add_argument('-s', '--save', action='store_true', help="Placeholder"),
-    'period':        lambda p: p.add_argument('-p', '--period', default='1Y', help="Placeholder"),
-    'units':         lambda p: p.add_argument('-u', '--units', help="Placeholder"),
-    'ticker':        lambda p: p.add_argument('ticker', help="Stock ticker symbol"),
-    'ticker_flag':   lambda p: p.add_argument('-t', '--ticker', required=True, help="Stock ticker symbol"),
-    'num':           lambda p: p.add_argument('-n', '--num', default=1000, help="Placeholder"),
-    'quarter':       lambda p: p.add_argument('-q', '--quarter', default='20', help="Placeholder"),
-    'metric':        lambda p: p.add_argument('-m', '--metric', default='eps', help="Placeholder"),
-    'model':         lambda p: p.add_argument('model', help="Placeholder"),
-    'plot':          lambda p: p.add_argument('-plt', '--plot', action='store_true', help="Placeholder"),
+    'save':          lambda p: p.add_argument('-s', '--save', action='store_true'),
+    'period':        lambda p: p.add_argument('-p', '--period', default='1Y'),
+    'units':         lambda p: p.add_argument('-u', '--units'),
+    'ticker':        lambda p: p.add_argument('ticker'),
+    'ticker_flag':   lambda p: p.add_argument('-t', '--ticker'),
+    'num':           lambda p: p.add_argument('-n', '--num', default=1000),
+    'quarter':       lambda p: p.add_argument('-q', '--quarter', default='20'),
+    'metric':        lambda p: p.add_argument('-m', '--metric', default='eps'),
+    'model':         lambda p: p.add_argument('model'),
+    'plot':          lambda p: p.add_argument('-plt', '--plot', action='store_true'),
+    'help':          lambda p: p.add_argument('-h', '--help', action='store_true')
 }
 
 def parse_args(arg_str, expected_args, prog_name='command'):
-    parser = argparse.ArgumentParser(prog=prog_name)
+    parser = argparse.ArgumentParser(prog=prog_name, add_help=False)
     for arg in expected_args:
         if arg not in ARGUMENTS:
             raise ValueError(f"Unknown arg type: {arg}")
