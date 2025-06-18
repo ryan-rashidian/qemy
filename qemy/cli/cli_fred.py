09d80efbc53e833a52afa81c4cb3c3b59da66803
ryan-rashidian <ryanrashidian@gmail.com>
2025-06-17 19:06:00 -0700

    ENH Added -h --help options to all FREDCmd methods in cli_fred.py

--- qemy/cli/cli_fred.py
+++ qemy/cli/cli_fred.py
@@ -23,7 +23,7 @@ class FREDCmd:
         if self.help:
             print_help_table(" rfr ", [
                 ("Info:", "Fetches 1 Year TBill Yield"),
-                ("Usage:", "rfr\n")
+                ("Usage:", "fred rfr\n")
             ])
 
         else:
@@ -36,7 +36,7 @@ class FREDCmd:
         if self.help:
             print_help_table(" cpi ", [
                 ("Info:", "Fetches Consumer Price Index data"),
-                ("Usage:", "cpi\n"),
+                ("Usage:", "fred cpi -p <PERIOD>\n"),
             ])
 
         else:
@@ -50,84 +50,140 @@ class FREDCmd:
                 print('For valid syntax, Try: cpi -p 1Y')
 
     def gdp(self):
-        units = 'pc1' if self.units is None else self.units
-        if isinstance(self.period, str):
-            try:
-                print(FREDData().get_gdp(period=self.period, units=units))
-            except Exception as e:
-                print(f"Could not fetch data ERROR:\n{e}")
+        if self.help:
+            print_help_table(" gdp ", [
+                ("Info:", "Fetches Gross Domestic Product data"),
+                ("Usage:", "fred gpd -p <PERIOD>\n"),
+            ])
+
         else:
-            print('For valid syntax, Try: gdp -p 1Y')
+            units = 'pc1' if self.units is None else self.units
+            if isinstance(self.period, str):
+                try:
+                    print(FREDData().get_gdp(period=self.period, units=units))
+                except Exception as e:
+                    print(f"Could not fetch data ERROR:\n{e}")
+            else:
+                print('For valid syntax, Try: gdp -p 1Y')
 
     def sent(self):
-        units = 'pch' if self.units is None else self.units
-        if isinstance(self.period, str):
-            try:
-                print(FREDData().get_sentiment(period=self.period, units=units))
-            except Exception as e:
-                print(f"Could not fetch data ERROR:\n{e}")
+        if self.help:
+            print_help_table(" sent ", [
+                ("Info:", "Fetches Consumer Sentiment Index data"),
+                ("Usage:", "fred sent -p <PERIOD>\n"),
+            ])
+
         else:
-            print('For valid syntax, Try: sent -p 1Y')
+            units = 'pch' if self.units is None else self.units
+            if isinstance(self.period, str):
+                try:
+                    print(FREDData().get_sentiment(period=self.period, units=units))
+                except Exception as e:
+                    print(f"Could not fetch data ERROR:\n{e}")
+            else:
+                print('For valid syntax, Try: sent -p 1Y')
 
     def nfp(self):
-        units = 'pc1' if self.units is None else self.units
-        if isinstance(self.period, str):
-            try:
-                print(FREDData().get_nf_payrolls(period=self.period, units=units))
-            except Exception as e:
-                print(f"Could not fetch data ERROR:\n{e}")
+        if self.help:
+            print_help_table(" nfp ", [
+                ("Info:", "Fetches Nonfarm Payroll data"),
+                ("Usage:", "fred nfp -p <PERIOD>\n"),
+            ])
+
         else:
-            print('For valid syntax, Try: nfp -p 1Y')
+            units = 'pc1' if self.units is None else self.units
+            if isinstance(self.period, str):
+                try:
+                    print(FREDData().get_nf_payrolls(period=self.period, units=units))
+                except Exception as e:
+                    print(f"Could not fetch data ERROR:\n{e}")
+            else:
+                print('For valid syntax, Try: nfp -p 1Y')
 
     def interest(self):
-        units = 'pc1' if self.units is None else self.units
-        if isinstance(self.period, str):
-            try:
-                print(FREDData().get_interest_rate(period=self.period, units=units))
-            except Exception as e:
-                print(f"Could not fetch data ERROR:\n{e}")
+        if self.help:
+            print_help_table(" interest ", [
+                ("Info:", "Fetches Fed Interest Rate data"),
+                ("Usage:", "fred interest -p <TICKER>\n"),
+            ])
+
         else:
-            print('For valid syntax, Try: interest -p 1Y')
+            units = 'pc1' if self.units is None else self.units
+            if isinstance(self.period, str):
+                try:
+                    print(FREDData().get_interest_rate(period=self.period, units=units))
+                except Exception as e:
+                    print(f"Could not fetch data ERROR:\n{e}")
+            else:
+                print('For valid syntax, Try: interest -p 1Y')
 
     def jobc(self):
-        units = 'pc1' if self.units is None else self.units
-        if isinstance(self.period, str):
-            try:
-                print(FREDData().get_jobless_claims(period=self.period, units=units))
-            except Exception as e:
-                print(f"Could not fetch data ERROR:\n{e}")
+        if self.help:
+            print_help_table(" jobc ", [
+                ("Info:", "Fetches Jobless Claim data"),
+                ("Usage:", "fred jobc -p <PERIOD>\n"),
+            ])
+
         else:
-            print('For valid syntax, Try: jobc -p 1Y')
+            units = 'pc1' if self.units is None else self.units
+            if isinstance(self.period, str):
+                try:
+                    print(FREDData().get_jobless_claims(period=self.period, units=units))
+                except Exception as e:
+                    print(f"Could not fetch data ERROR:\n{e}")
+            else:
+                print('For valid syntax, Try: jobc -p 1Y')
 
     def unem(self):
-        units = 'pc1' if self.units is None else self.units
-        if isinstance(self.period, str):
-            try:
-                print(FREDData().get_unemployment(period=self.period, units=units))
-            except Exception as e:
-                print(f"Could not fetch data ERROR:\n{e}")
+        if self.help:
+            print_help_table(" unem ", [
+                ("Info:", "Fetches Unemployment Rate data"),
+                ("Usage:", "fred unem -p <PERIOD>\n"),
+            ])
+
         else:
-            print('For valid syntax, Try: unem -p 1Y')
+            units = 'pc1' if self.units is None else self.units
+            if isinstance(self.period, str):
+                try:
+                    print(FREDData().get_unemployment(period=self.period, units=units))
+                except Exception as e:
+                    print(f"Could not fetch data ERROR:\n{e}")
+            else:
+                print('For valid syntax, Try: unem -p 1Y')
 
     def indp(self):
-        units = 'pc1' if self.units is None else self.units
-        if isinstance(self.period, str):
-            try:
-                print(FREDData().get_industrial_production(period=self.period, units=units))
-            except Exception as e:
-                print(f"Could not fetch data ERROR:\n{e}")
+        if self.help:
+            print_help_table(" indp ", [
+                ("Info:", "Fetches Industrial Production data"),
+                ("Usage:", "fred indp -p <PERIOD>\n"),
+            ])
+
         else:
-            print('For valid syntax, Try: indp -p 1Y')
+            units = 'pc1' if self.units is None else self.units
+            if isinstance(self.period, str):
+                try:
+                    print(FREDData().get_industrial_production(period=self.period, units=units))
+                except Exception as e:
+                    print(f"Could not fetch data ERROR:\n{e}")
+            else:
+                print('For valid syntax, Try: indp -p 1Y')
 
     def netex(self):
-        units = 'lin' if self.units is None else self.units 
-        if isinstance(self.period, str):
-            try:
-                print(FREDData().get_net_exports(period=self.period, units=units.lower()))
-            except Exception as e:
-                print(f"Could not fetch data ERROR:\n{e}")
+        if self.help:
+            print_help_table(" netex ", [
+                ("Info:", "Fetches Net Export data"),
+                ("Usage:", "fred netex -p <PERIOD>\n"),
+            ])
+
         else:
-            print('For valid syntax, Try: netex -p 1Y')
+            units = 'lin' if self.units is None else self.units 
+            if isinstance(self.period, str):
+                try:
+                    print(FREDData().get_net_exports(period=self.period, units=units.lower()))
+                except Exception as e:
+                    print(f"Could not fetch data ERROR:\n{e}")
+            else:
+                print('For valid syntax, Try: netex -p 1Y')
 
     def run(self):
         if self.help_requested:
