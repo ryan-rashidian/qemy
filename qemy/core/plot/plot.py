8d7d5f0e6f5b6b09a75f657e02dc8c864412f048
ryan-rashidian <ryanrashidian@gmail.com>
2025-06-19 15:13:09 -0700

    REF Renamed variables in plot.py for clarity

--- qemy/core/plot/plot.py
+++ qemy/core/plot/plot.py
@@ -28,17 +28,17 @@ def plot_models(ticker, title='title', save=False, plot_func=None):
         plt.show()
 
 def plot_price(ticker, period):
-    data = StockMarket().get_prices(ticker=ticker, period=period)
-    df = pd.DataFrame(data)
-    df['date'] = pd.to_datetime(df['date'])
+    stock_data = StockMarket().get_prices(ticker=ticker, period=period)
+    stock_df = pd.DataFrame(stock_data)
+    stock_df['date'] = pd.to_datetime(stock_df['date'])
 
     plt.figure(figsize=(14, 8))
-    plt.plot(df['date'], df['close'], label='Price', color='green', linewidth=3, marker= 'o', alpha=0.8)
-    if len(df['date']) >= 250:
-        df['50_DMA'] = df['close'].rolling(window=50).mean()
-        df['200_DMA'] = df['close'].rolling(window=200).mean()
-        plt.plot(df['date'], df['50_DMA'], label='50 DMA', color='blue', linewidth=1, alpha=0.5)
-        plt.plot(df['date'], df['200_DMA'], label='200 DMA', color='red', linewidth=2, alpha=0.5)
+    plt.plot(stock_df['date'], stock_df['close'], label='Price', color='green', linewidth=3, marker= 'o', alpha=0.8)
+    if len(stock_df['date']) >= 250:
+        stock_df['50_DMA'] = stock_df['close'].rolling(window=50).mean()
+        stock_df['200_DMA'] = stock_df['close'].rolling(window=200).mean()
+        plt.plot(stock_df['date'], stock_df['50_DMA'], label='50 DMA', color='blue', linewidth=1, alpha=0.5)
+        plt.plot(stock_df['date'], stock_df['200_DMA'], label='200 DMA', color='red', linewidth=2, alpha=0.5)
     plt.xlabel('Date')
     plt.ylabel('Price (Log-Scale)')
     plt.yscale('log')
@@ -49,11 +49,11 @@ def plot_price(ticker, period):
     plt.show()
 
 def plot_cpi(period, save=False, units='pc1'):
-    data = FREDData().get_cpi(period=period, units=units)
-    df = pd.DataFrame(data)
+    cpi_data = FREDData().get_cpi(period=period, units=units)
+    cpi_df = pd.DataFrame(cpi_data)
 
     plt.figure(figsize=(14, 8))
-    plt.plot(df.index, df['value'], label='CPI Inflation', color='green', linewidth=3, marker= 'o', alpha=0.8)
+    plt.plot(cpi_df.index, cpi_df['value'], label='CPI Inflation', color='green', linewidth=3, marker= 'o', alpha=0.8)
     plt.xlabel('Date')
     plt.ylabel('Inflation')
     plt.title(f"CPI Inflation: {units}")
@@ -68,11 +68,11 @@ def plot_cpi(period, save=False, units='pc1'):
         plt.show()
     
 def plot_gdp(period, save=False, units='pc1'):
-    data = FREDData().get_gdp(period=period, units=units)
-    df = pd.DataFrame(data)
+    gdp_data = FREDData().get_gdp(period=period, units=units)
+    gdp_df = pd.DataFrame(gdp_data)
 
     plt.figure(figsize=(14, 8))
-    plt.plot(df.index, df['value'], label='GDP', color='green', linewidth=3, marker= 'o', alpha=0.8)
+    plt.plot(gdp_df.index, gdp_df['value'], label='GDP', color='green', linewidth=3, marker= 'o', alpha=0.8)
     plt.xlabel('Date')
     plt.ylabel('GDP')
     plt.title(f"GDP: {units}")
@@ -87,11 +87,11 @@ def plot_gdp(period, save=False, units='pc1'):
         plt.show()
 
 def plot_sentiment(period, save=False, units='pch'):
-    data = FREDData().get_sentiment(period=period, units=units)
-    df = pd.DataFrame(data)
+    sentiment_data = FREDData().get_sentiment(period=period, units=units)
+    sentiment_df = pd.DataFrame(sentiment_data)
 
     plt.figure(figsize=(14, 8))
-    plt.plot(df.index, df['value'], label='Sentiment', color='green', linewidth=3, marker= 'o', alpha=0.8)
+    plt.plot(sentiment_df.index, sentiment_df['value'], label='Sentiment', color='green', linewidth=3, marker= 'o', alpha=0.8)
     plt.xlabel('Date')
     plt.ylabel('Sentiment')
     plt.title(f"Sentiment: {units}")
@@ -106,11 +106,11 @@ def plot_sentiment(period, save=False, units='pch'):
         plt.show()
 
 def plot_nfp(period, save=False, units='pc1'):
-    data = FREDData().get_nf_payrolls(period=period, units=units)
-    df = pd.DataFrame(data)
+    nfp_data = FREDData().get_nf_payrolls(period=period, units=units)
+    nfp_df = pd.DataFrame(nfp_data)
 
     plt.figure(figsize=(14, 8))
-    plt.plot(df.index, df['value'], label='NFP', color='green', linewidth=3, marker= 'o', alpha=0.8)
+    plt.plot(nfp_df.index, nfp_df['value'], label='NFP', color='green', linewidth=3, marker= 'o', alpha=0.8)
     plt.xlabel('Date')
     plt.ylabel('NFP')
     plt.title(f"Non-Farm Payrolls: {units}")
@@ -125,11 +125,11 @@ def plot_nfp(period, save=False, units='pc1'):
         plt.show()
 
 def plot_interest(period, save=False, units='pc1'):
-    data = FREDData().get_interest_rate(period=period, units=units)
-    df = pd.DataFrame(data)
+    interest_data = FREDData().get_interest_rate(period=period, units=units)
+    interest_df = pd.DataFrame(interest_data)
 
     plt.figure(figsize=(14, 8))
-    plt.plot(df.index, df['value'], label='interest', color='green', linewidth=3, marker= 'o', alpha=0.8)
+    plt.plot(interest_df.index, interest_df['value'], label='interest', color='green', linewidth=3, marker= 'o', alpha=0.8)
     plt.xlabel('Date')
     plt.ylabel('Interest')
     plt.title(f"Interest: {units}")
@@ -144,11 +144,11 @@ def plot_interest(period, save=False, units='pc1'):
         plt.show()
 
 def plot_jobc(period, save=False, units='pc1'):
-    data = FREDData().get_jobless_claims(period=period, units=units)
-    df = pd.DataFrame(data)
+    jobc_data = FREDData().get_jobless_claims(period=period, units=units)
+    jobc_df = pd.DataFrame(jobc_data)
 
     plt.figure(figsize=(14, 8))
-    plt.plot(df.index, df['value'], label='jobless claims', color='green', linewidth=3, marker= 'o', alpha=0.8)
+    plt.plot(jobc_df.index, jobc_df['value'], label='jobless claims', color='green', linewidth=3, marker= 'o', alpha=0.8)
     plt.xlabel('Date')
     plt.ylabel('Jobless Claims')
     plt.title(f"Jobless Claims: {units}")
@@ -163,11 +163,11 @@ def plot_jobc(period, save=False, units='pc1'):
         plt.show()
 
 def plot_unemployment(period, save=False, units='pc1'):
-    data = FREDData().get_unemployment(period=period, units=units)
-    df = pd.DataFrame(data)
+    unem_data = FREDData().get_unemployment(period=period, units=units)
+    unem_df = pd.DataFrame(unem_data)
 
     plt.figure(figsize=(14, 8))
-    plt.plot(df.index, df['value'], label='unemployment', color='green', linewidth=3, marker= 'o', alpha=0.8)
+    plt.plot(unem_df.index, unem_df['value'], label='unemployment', color='green', linewidth=3, marker= 'o', alpha=0.8)
     plt.xlabel('Date')
     plt.ylabel('Unemployment rate')
     plt.title(f"Unemployment rate: {units}")
@@ -182,11 +182,11 @@ def plot_unemployment(period, save=False, units='pc1'):
         plt.show()
 
 def plot_ind_prod(period, save=False, units='pc1'):
-    data = FREDData().get_industrial_production(period=period, units=units)
-    df = pd.DataFrame(data)
+    indp_data = FREDData().get_industrial_production(period=period, units=units)
+    indp_df = pd.DataFrame(indp_data)
 
     plt.figure(figsize=(14, 8))
-    plt.plot(df.index, df['value'], label='ind prod', color='green', linewidth=3, marker= 'o', alpha=0.8)
+    plt.plot(indp_df.index, indp_df['value'], label='ind prod', color='green', linewidth=3, marker= 'o', alpha=0.8)
     plt.xlabel('Date')
     plt.ylabel('Ind Prod')
     plt.title(f"Industrial Production: {units}")
@@ -201,11 +201,11 @@ def plot_ind_prod(period, save=False, units='pc1'):
         plt.show()
 
 def plot_netex(period, save=False, units='lin'):
-    data = FREDData().get_net_exports(period=period, units=units)
-    df = pd.DataFrame(data)
+    netex_data = FREDData().get_net_exports(period=period, units=units)
+    netex_df = pd.DataFrame(netex_data)
 
     plt.figure(figsize=(14, 8))
-    plt.plot(df.index, df['value'], label='NetEx', color='green', linewidth=3, marker= 'o', alpha=0.8)
+    plt.plot(netex_df.index, netex_df['value'], label='NetEx', color='green', linewidth=3, marker= 'o', alpha=0.8)
     plt.xlabel('Date')
     plt.ylabel('NetEx')
     plt.title(f"Real Net Exports of Goods and Services: {units}")
