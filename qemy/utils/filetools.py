913b21c55c5f388554939386456c0975d7bf8d1b
ryan-rashidian <ryanrashidian@gmail.com>
2025-06-21 14:25:51 -0700

    ENH Added save_to_png and save_to_csv functions to filetools.py

--- qemy/utils/filetools.py
+++ qemy/utils/filetools.py
@@ -1,4 +1,7 @@
 from pathlib import Path
+import matplotlib.pyplot as plt
+import pandas as pd
+import config as cfg
 
 def get_next_path(base_dir: Path, name="file", ext="png") -> Path:
     i = 1
@@ -7,4 +10,13 @@ def get_next_path(base_dir: Path, name="file", ext="png") -> Path:
         if not check.exists():
             return check
         i += 1
+
+def save_to_png(filename):
+    output_path =  get_next_path(cfg.EXPORT_CHART_DIR, name=filename, ext='png')
+    plt.savefig(output_path)
+
+def save_to_csv(df: pd.DataFrame):
+    output_path =  get_next_path(cfg.EXPORT_TABLE_DIR, name='table', ext='csv')
+    df.reset_index(inplace=True)
+    df.to_csv(output_path, index=False)
 
