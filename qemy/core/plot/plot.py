61cb486b9261d675811b7d13c6f2eeb3a798f2c4
ryan-rashidian <ryanrashidian@gmail.com>
2025-06-23 23:06:39 -0700

    FIX Removed un-used argument from plot_models in plot.py

--- qemy/core/plot/plot.py
+++ qemy/core/plot/plot.py
@@ -3,7 +3,7 @@ import pandas as pd
 from qemy.utils.filetools import save_to_png
 from qemy.data.api_tiingo import StockMarket
 
-def plot_models(ticker, title='title', save=False, plot_func=None):
+def plot_models(title='title', save=False, plot_func=None):
     plt.figure(figsize=(14, 8))
     if plot_func:
         plot_func()
