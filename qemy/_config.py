14cfa89d0ba9771f59f2b45a198af79e68724ddb
ryan-rashidian <ryanrashidian@gmail.com>
2025-06-28 16:01:34 -0700

    ENH Added get_env_str function in _config.py to cast API keys explicitly to str

--- qemy/_config.py
+++ qemy/_config.py
@@ -5,12 +5,20 @@ from dotenv import load_dotenv
 
 load_dotenv(dotenv_path=Path(__file__).resolve().parents[1] / '.env')
 
-# KEY
-FRED_API_KEY = os.getenv('FRED_API_KEY')
-TIINGO_API_KEY = os.getenv('TIINGO_API_KEY')
-EDGAR_USER_AGENT = os.getenv('EDGAR_USER_AGENT')
+# === KEY ===
+def get_env_str(key: str) -> str:
+    val = os.getenv(key)
+    if val is None:
+        print(f"Missing API key: {key}")
+        print(f"Exiting Qemy, Please setup API keys correctly.")
+        sys.exit(1)
+    return val
 
-# URL
+FRED_API_KEY: str = get_env_str('FRED_API_KEY')
+TIINGO_API_KEY: str = get_env_str('TIINGO_API_KEY')
+EDGAR_USER_AGENT: str = get_env_str('EDGAR_USER_AGENT')
+
+# === URL ===
 FRED_URL = "https://api.stlouisfed.org/fred/series/observations"
 
 TIINGO_URL = "https://api.tiingo.com/tiingo/daily/"
@@ -20,7 +28,7 @@ EDGAR_CIK_URL = "https://www.sec.gov/files/company_tickers.json"
 EDGAR_FACTS_URL = "https://data.sec.gov/api/xbrl/companyfacts/CIK"
 EDGAR_ZIP_URL = "https://www.sec.gov/Archives/edgar/daily-index/xbrl/companyfacts.zip"
 
-# PATH
+# === PATH ===
 if getattr(sys, 'frozen', False):
     PROJECT_ROOT = Path(sys.executable).resolve().parent
 else:
@@ -34,3 +42,4 @@ EXPORT_TABLE_DIR.mkdir(parents=True, exist_ok=True)
 
 BULK_DIR = PROJECT_ROOT / "bulk_data"
 BULK_DIR.mkdir(parents=True, exist_ok=True)
+
