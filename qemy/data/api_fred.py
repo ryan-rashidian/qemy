d574ba0fd04b8716bf8cff2ea5306e85a6bdafca
ryan-rashidian <ryanrashidian@gmail.com>
2025-07-04 14:29:48 -0700

    REF Add FREDClient type hints, docstrings, and logging

--- qemy/data/api_fred.py
+++ qemy/data/api_fred.py
@@ -1,21 +1,56 @@
+"""FRED API module.
+
+This module requests and fetches data from FRED API.
+"""
+
+import logging
 import pandas as pd
 
 from ._api_tools import safe_status_get, parse_period
 
 from qemy import _config as cfg
 
+logger = logging.getLogger(__name__)
+
 class FREDClient:
+    """Client for fetching macro-economic data from FRED API."""
+
     def __init__(self):
-        self.API_KEY = cfg.fred_api_key()
-        self.url = cfg.FRED_URL
+        """Initialize FREDClient with API key and URL."""
+        self.API_KEY: str = cfg.fred_api_key()
+        self.url: str = cfg.FRED_URL
 
     def _fetch_series(
-            self, series_id, period='1Y', 
-            frequency='m', units='pc1', 
-            aggregation='avg', limit=None
-    ):
+        self, 
+        series_id: str, 
+        period: str='1Y', 
+        frequency: str='m', 
+        units: str='pc1', 
+        aggregation: str='avg', 
+        limit: int=100_000
+    ) -> pd.DataFrame | None:
+        """Internal method for sending requests.
 
+        Args:
+            series_id (str): Series ID for observation
+            period (str): Period for requested data
+                - (e.g., "1Y", "3M")
+            frequency (str): Frequency for observations 
+                - (e.g., "m", "q", "d")
+            units (str): Units of measurement for observations
+                - (e.g., "pc1", "pch", "lin")
+            aggregation (str): Frequency aggregation 
+                - "avg" Average
+                - "sum" Sum
+                - "eop" End of Period
+            limit (int): Integer to slice tail of observations
+
+        Returns:
+            pd.DataFrame: DataFrame indexed by 'date' with 'val' column 
+            None: Failed to fetch data from FRED API
+        """
         start_date, end_date = parse_period(period)
+
         params = {
             'series_id': series_id,
             'api_key': self.API_KEY,
@@ -25,54 +60,183 @@ class FREDClient:
             'observation_end': end_date,
             'frequency': frequency,
             'units': units,
-            'aggreation_model': aggregation,
+            'aggregation_method': aggregation,
+            'limit': limit
         }
-        if limit:
-            params['limit'] = limit
 
         try:
             fred_data = safe_status_get(url=self.url, params=params)
             if fred_data and fred_data.get('observations'):
+                logger.info("FRED API request Successful")
+
                 obs_df = pd.DataFrame(fred_data['observations'])
                 obs_df['date'] = pd.to_datetime(obs_df['date'])
                 obs_df.set_index('date', inplace=True)
-                obs_df['value'] = pd.to_numeric(obs_df['value'], errors='coerce')
+                obs_df['value'] = pd.to_numeric(
+                    obs_df['value'], 
+                    errors='coerce'
+                )
+                # Drop un-needed columns
                 return obs_df.drop(
                     columns=['realtime_start', 'realtime_end'], 
                     errors='ignore'
                 )
 
         except Exception as e:
-            print(f"data/api_fred.py Error:\n{e}")
+            logger.exception(f"FRED API request Failed\n{e}")
         return None
 
     def get_tbill_yield(self):
-        return self._fetch_series('GS1', period='1M', frequency='m', units='lin', limit=1)
+        """Fetch latest observation for: 1 Year T-Bill Yield."""
+        return self._fetch_series(
+            series_id='GS1', 
+            period='1M', 
+            frequency='m', 
+            units='lin', 
+            limit=1
+        )
 
-    def get_cpi(self, period='1Y', units='pc1'):
-        return self._fetch_series('CPIAUCSL', period, frequency='m', units=units)
+    def get_cpi(self, period: str='1Y', units: str='pc1'):
+        """Fetch observations for: Consumer Price Index.
 
-    def get_gdp(self, period='1Y', units='pc1'):
-        return self._fetch_series('GDP', period, frequency='q', units=units)
+        Args:
+            period (str): Period for requested data
+                - (e.g., "1Y", "3M")
+            units (str): Units of measurement for observations
+                - (e.g., "pc1", "pch", "lin")
+        """
+        return self._fetch_series(
+            series_id='CPIAUCSL', 
+            period=period, 
+            frequency='m', 
+            units=units
+        )
 
-    def get_sentiment(self, period='1Y', units='pch'):
-        return self._fetch_series('UMCSENT', period, frequency='m', units=units)
+    def get_gdp(self, period: str='1Y', units: str='pc1'):
+        """Fetch observations for: Gross Domestic Product.
 
-    def get_nf_payrolls(self, period='1Y', units='pc1'):
-        return self._fetch_series('PAYEMS', period, frequency='m', units=units)
+        Args:
+            period (str): Period for requested data
+                - (e.g., "1Y", "3M")
+            units (str): Units of measurement for observations
+                - (e.g., "pc1", "pch", "lin")
+        """
+        return self._fetch_series(
+            series_id='GDP', 
+            period=period, 
+            frequency='q', 
+            units=units
+        )
 
-    def get_interest_rate(self, period='1Y', units='pc1'):
-        return self._fetch_series('DFF', period, frequency='d', units=units)
+    def get_sentiment(self, period: str='1Y', units: str='pch'):
+        """Fetch observations for: Consumer Sentiment Index.
 
-    def get_jobless_claims(self, period='1Y', units='pc1'):
-        return self._fetch_series('ICSA', period, frequency='w', units=units)
+        Args:
+            period (str): Period for requested data
+                - (e.g., "1Y", "3M")
+            units (str): Units of measurement for observations
+                - (e.g., "pc1", "pch", "lin")
+        """
+        return self._fetch_series(
+            series_id='UMCSENT', 
+            period=period, 
+            frequency='m', 
+            units=units
+        )
 
-    def get_unemployment(self, period='1Y', units='pc1'):
-        return self._fetch_series('UNRATE', period, frequency='m', units=units)
+    def get_nf_payrolls(self, period: str='1Y', units: str='pc1'):
+        """Fetch observations for: Nonfarm Payrolls.
 
-    def get_industrial_production(self, period='1Y', units='pc1'):
-        return self._fetch_series('INDPRO', period, frequency='m', units=units)
+        Args:
+            period (str): Period for requested data
+                - (e.g., "1Y", "3M")
+            units (str): Units of measurement for observations
+                - (e.g., "pc1", "pch", "lin")
+        """
+        return self._fetch_series(
+            series_id='PAYEMS', 
+            period=period, 
+            frequency='m', 
+            units=units
+        )
 
-    def get_net_exports(self, period='1Y', units='lin'):
-        return self._fetch_series('NETEXC', period, frequency='q', units=units)        
+    def get_interest_rate(self, period: str='1Y', units: str='pc1'):
+        """Fetch observations for: Federal Interest Rate.
+
+        Args:
+            period (str): Period for requested data
+                - (e.g., "1Y", "3M")
+            units (str): Units of measurement for observations
+                - (e.g., "pc1", "pch", "lin")
+        """
+        return self._fetch_series(
+            series_id='DFF', 
+            period=period, 
+            frequency='d', 
+            units=units
+        )
+
+    def get_jobless_claims(self, period: str='1Y', units: str='pc1'):
+        """Fetch observations for: Jobless Claims.
+
+        Args:
+            period (str): Period for requested data
+                - (e.g., "1Y", "3M")
+            units (str): Units of measurement for observations
+                - (e.g., "pc1", "pch", "lin")
+        """
+        return self._fetch_series(
+            series_id='ICSA', 
+            period=period, 
+            frequency='w', 
+            units=units
+        )
+
+    def get_unemployment(self, period: str='1Y', units: str='pc1'):
+        """Fetch observations for: Unemployment Rate.
+
+        Args:
+            period (str): Period for requested data
+                - (e.g., "1Y", "3M")
+            units (str): Units of measurement for observations
+                - (e.g., "pc1", "pch", "lin")
+        """
+        return self._fetch_series(
+            series_id='UNRATE', 
+            period=period, 
+            frequency='m', 
+            units=units
+        )
+
+    def get_industrial_production(self, period: str='1Y', units: str='pc1'):
+        """Fetch observations for: Industrial Production Index.
+
+        Args:
+            period (str): Period for requested data
+                - (e.g., "1Y", "3M")
+            units (str): Units of measurement for observations
+                - (e.g., "pc1", "pch", "lin")
+        """
+        return self._fetch_series(
+            series_id='INDPRO', 
+            period=period, 
+            frequency='m', 
+            units=units
+        )
+
+    def get_net_exports(self, period: str='1Y', units: str='lin'):
+        """Fetch observations for: Net Exports.
+
+        Args:
+            period (str): Period for requested data
+                - (e.g., "1Y", "3M")
+            units (str): Units of measurement for observations
+                - (e.g., "pc1", "pch", "lin")
+        """
+        return self._fetch_series(
+            series_id='NETEXC', 
+            period=period, 
+            frequency='q', 
+            units=units
+        )        
 
