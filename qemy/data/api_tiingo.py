d9dd0919d034157b67ef8d8212da1994a766273c
ryan-rashidian <ryanrashidian@gmail.com>
2025-07-07 22:24:45 -0700

    ENH Added _normalize_tickers method in TiingoClient to handle ticker argument initialization

--- qemy/data/api_tiingo.py
+++ qemy/data/api_tiingo.py
@@ -2,40 +2,91 @@
 
 This module requests and fetches data from Tiingo API.
 """
-
 import logging
 
 import pandas as pd
 
 from qemy import _config as cfg
 
-from ._api_tools import parse_period, safe_status_get
+from ._api_tools import ClientError, parse_period, safe_status_get
 
 logger = logging.getLogger(__name__)
 
 class TiingoClient:
     """Client for fetching stock market data from Tiingo API."""
 
-    def __init__(self, ticker: str):
+    def __init__(self, ticker: str | list[str]):
         """Initialize TiingoClient with API key and request headers.
 
         Args:
-            tickers (str): Company ticker symbol
+            ticker (str | list[str]): Company ticker symbol(s)
+
+        Raises:
+            ClientError: If self.tickers cannot be defined
         """
-        self.ticker = ticker
+        self.tickers = self._normalize_tickers(ticker)
+        self.ticker = self.tickers[0]
+
         self.API_KEY: str = cfg.tiingo_api_key()
         self.HEADERS: dict[str, str] = {
             'Content-Type': 'application/json',
             'Authorization': f"Token {self.API_KEY}"
         }
 
+    def _normalize_tickers(
+        self,
+        ticker: str | list[str] | object
+    ) -> list[str]:
+        """Internal method for normalizing ticker argument.
+
+        Args:
+            ticker (str | list[str] | object): Company ticker symbol(s)
+
+        Returns:
+            list[str]: Normalized ticker list for self.tickers
+
+        Raises:
+            ClientError: If ticker argument is invalid type
+        """
+        if isinstance(ticker, str):
+            if not ticker:
+                raise ClientError("Args (ticker): empty string")
+            return [ticker.strip().upper()]
+
+        elif isinstance(ticker, list):
+            if not ticker:
+                raise ClientError("Args (ticker): empty list")
+
+            normalized_tickers = []
+
+            for i, t in enumerate(ticker):
+                if not isinstance(t, str):
+                    raise ClientError(
+                        f"Args (ticker[{i}]): incorrect type: {type(t)}"
+                    )
+
+                clean_ticker = t.strip().upper()
+                if not clean_ticker:
+                    raise ClientError(f"Args (ticker[{i}]): empty string")
+                normalized_tickers.append(clean_ticker)
+
+            return normalized_tickers
+
+        else:
+            logger.error(f"Failed to initialize agument: {ticker}")
+            raise ClientError(
+                f"Args (ticker): incorrect type: {type(ticker)}"
+            )
+
     def get_prices(
         self,
         period: str = '1W',
-        resample = 'daily',
+        resample: str = 'daily',
         columns: str | list[str] = 'adjClose'
     ) -> pd.DataFrame:
         """Fetch historical price data for a given ticker.
+
+        This module will only return data for the first[0] ticker string.
 
         Args:
             period (str): Period (e.g., "1Y", "3M", "50D")
@@ -46,11 +97,6 @@ class TiingoClient:
             pd.DataFrame: DataFrame with pd.DatetimeIndex and column for values
         """
         start_date, end_date = parse_period(period)
-
-        if isinstance(self.ticker, list):
-            logger.error("")
-            return pd.DataFrame()
-
 
         url = f"{cfg.TIINGO_URL}{self.ticker}/prices"
         params = {
@@ -71,7 +117,6 @@ class TiingoClient:
             return pd.DataFrame()
         logger.info("Tiingo API request Successful")
 
-        # DataFrame formatting step
         price_df = pd.DataFrame(price_data)
         price_df['date'] = pd.to_datetime(price_df['date'])
         price_df.set_index('date', inplace=True)
@@ -79,22 +124,13 @@ class TiingoClient:
 
         return price_df if not price_df.empty else pd.DataFrame()
 
-    def get_quote(self, ticker_lst: list[str]) -> dict[str, dict]:
+    def get_quote(self) -> dict[str, dict]:
         """Fetch latest quote data for one or more tickers.
-
-        Args:
-            ticker_lst (list[str]):
 
         Returns:
             dict[str, dict]: With tickers mapped to their quote data
         """
-        tickers = ticker_lst
-
-        if isinstance(self.ticker, str):
-            # Normalize self.ticker arg to list
-            tickers = [self.ticker]
-
-        url = f"{cfg.TIINGO_IEX_URL}{','.join(tickers)}"
+        url = f"{cfg.TIINGO_IEX_URL}{','.join(self.tickers)}"
 
         response = safe_status_get(url=url, headers=self.HEADERS)
         if not isinstance(response, list):
