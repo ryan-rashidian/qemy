2a4def01fdcd2d1a66b6409856a9c6037407f3c9
ryan-rashidian <ryanrashidian@gmail.com>
2025-07-09 20:43:28 -0700

    REF Raise errors and add docstrings, and type hints in _api_tools module

--- qemy/data/_api_tools.py
+++ qemy/data/_api_tools.py
@@ -1,3 +1,5 @@
+"""Common tools and utilities for /data/ package."""
+
 import logging
 from datetime import datetime
 from pathlib import Path
@@ -8,10 +10,18 @@ from dateutil.relativedelta import relativedelta
 logger = logging.getLogger(__name__)
 
 class ClientError(Exception):
-    """Custom exception for Qemy Client classes"""
+    """Custom exception for Qemy Client classes."""
     pass
 
-def parse_period(period_str):
+def parse_period(period_str) -> tuple[str, str]:
+    """Convert shorthand period into formatted date-time strings.
+
+    Args:
+        period_str (str): Shorthand period string (e.g., "2W", "4M")
+
+    Returns:
+        tuple[str, str]: Tuple of start and end date strings
+    """
     now = datetime.now()
 
     try:
@@ -28,35 +38,73 @@ def parse_period(period_str):
             case 'Y':
                 start_date = now - relativedelta(years=value)
             case _:
-                raise ValueError("Invalid unit")
+                raise ClientError("Invalid unit")
 
         return start_date.strftime('%Y-%m-%d'), now.strftime('%Y-%m-%d')
 
-    except ValueError as err:
+    except ValueError as e:
         raise ClientError(
-            "Invalid period format.\nUse: D, W, M, or Y\n"
+            "Invalid period format.\n"
+            "Use: D, W, M, or Y\n"
             "Usage: <INTEGER><UNITS>\n"
             "Example: '6M', '2Y', '5D'"
-        ) from err
+        ) from e
 
-def safe_status_get(url, headers=None, params=None):
+def safe_status_get(
+    url: str,
+    headers: dict[str, str] | None=None,
+    params: dict[str, str] | None=None
+) -> dict:
+    """Request with error handling.
+
+    Args:
+        url (str): Request URL
+        headers (dict[str, str]): Request headers
+        params (dict[str, str]): Request parameters
+
+    Returns:
+        dict: Response data in dictionary format
+
+    Raises:
+        ClientError: If request fails
+    """
     response = None
     try:
         response = requests.get(url=url, headers=headers, params=params)
         response.raise_for_status()
         return response.json()
+
     except requests.exceptions.HTTPError as e:
-        logger.error(f"HTTPError: {e}")
+        logger.error(f"HTTP Error:\n{e}")
+        raise ClientError("HTTP Error") from e
+
     except requests.exceptions.RequestException as e:
         logger.error(f"Request failed:\n{e}")
-    except Exception as e:
-        logger.exception(f"Error: {e}")
-    return None
+        raise ClientError("Request failed") from e
 
-def safe_status_download(url, headers=None, dest_path=None):
+    except Exception as e:
+        logger.exception(f"Error:\n{e}")
+        raise ClientError("Unexpected Error") from e
+
+def safe_status_download(
+    url: str,
+    headers: dict[str, str] | None=None,
+    dest_path: Path | None=None
+):
+    """Request download with error handling.
+
+    Args:
+        url (str): Request URL
+        headers (dict[str, str]): Request headers
+        dest_path (Path): Download directory path
+
+    Raises:
+        ClientError: If download fails
+    """
     if not isinstance(dest_path, Path):
-        logger.error("Download failed: dest_path must be valid Path object")
-        return False
+        logger.error("Incorrect type for: dest_path")
+        raise ClientError("Incorrect type for: dest_path")
+
     try:
         with requests.get(url=url, headers=headers, stream=True) as r:
             r.raise_for_status()
@@ -64,12 +112,16 @@ def safe_status_download(url, headers=None, dest_path=None):
                 with open(dest_path, 'wb') as f:
                     for chunk in r.iter_content(chunk_size=8192):
                         f.write(chunk)
-        return True
+
     except requests.exceptions.HTTPError as e:
-        logger.error(f"HTTPError: {e}")
+        logger.error(f"HTTP Error during download:\n{e}")
+        raise ClientError("HTTP Error") from e
+
     except requests.exceptions.RequestException as e:
-        logger.error(f"Request failed: {e}")
+        logger.error(f"Request failed:\n{e}")
+        raise ClientError("Request failed") from e
+
     except Exception as e:
-        logger.exception(f"Error: {e}")
-    return False
+        logger.exception(f"Error:\n{e}")
+        raise ClientError("Unexpected Error") from e
 
