2a471f9454bb8a1e62c5168c5e08f64a2c258b7c
ryan-rashidian <ryanrashidian@gmail.com>
2025-07-14 14:56:57 -0700

    ENH Centralize /metrics/ imports in dunder init

---
+++ qemy/core/metrics/__init__.py
@@ -0,0 +1,13 @@
+from .growth import cagr, growth_rate
+from .risk import max_dd, ratio_sharpe, volatility
+from .value import ratio_pb, ratio_pe
+
+__all__ = [
+    'cagr',
+    'growth_rate',
+    'ratio_sharpe',
+    'max_dd',
+    'volatility',
+    'ratio_pe',
+    'ratio_pb'
+]
