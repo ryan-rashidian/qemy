fba1fb30dfd331182a5ef54b1a429199926e9da4
ryan-rashidian <ryanrashidian@gmail.com>
2025-07-15 14:16:37 -0700

    REF Renamed plugin_base module to plugin_tools

--- qemy/plugins/__init__.py
+++ qemy/plugins/__init__.py
@@ -1,7 +1,8 @@
-from .plugin_base import BasePlugin
 from .plugin_loader import load_plugins
+from .plugin_tools import BasePlugin, PluginError
 
 __all__ = [
     'BasePlugin',
+    'PluginError',
     'load_plugins'
 ]
