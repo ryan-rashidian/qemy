951aa0e54ceb6e969c6dee38fc3a5c1a7dd66744
ryan-rashidian <ryanrashidian@gmail.com>
2025-07-17 17:01:02 -0700

    ENH Import /core/ into dunder main Python REPL entry

--- qemy/__main__.py
+++ qemy/__main__.py
@@ -1,3 +1,6 @@
 from .data import *  # noqa: F403
+from .core.metrics import * # noqa: F403
+from .core.models import * # noqa: F403
+from .core.plot import * # noqa: F403
 
 print("Qemy 0.1.1 Python REPL")
