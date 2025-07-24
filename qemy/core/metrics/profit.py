1fd67173411727cde651f965b050b256abbe3b9c
ryan-rashidian <ryanrashidian@gmail.com>
2025-07-24 14:32:44 -0700

    NEW Add ratio_roic function for 'return on invested capital ratio' in profit module

--- qemy/core/metrics/profit.py
+++ qemy/core/metrics/profit.py
@@ -77,8 +77,45 @@ def ratio_roa(ticker: str) -> dict:
         'roe': roa
     }
 
-def ratio_roic():
-    return
+def ratio_roic(ticker: str) -> dict:
+    """Calculate Return on Invested Capital ratio for given ticker.
+
+    Args:
+        ticker (str): Company ticker symbol
+
+    Returns:
+        dict: With ticker 'roic' key and corresponding value
+    """
+    TAX_RATE = 0.21 # estimate for US corporate tax rate
+    client = EDGARClient(ticker)
+    try:
+        operating_income_df = client.get_concept(concept='opinc')
+        operating_income = operating_income_df['val'].iloc[-1]
+        # Calculate NOPAT "Net Operating Income After Tax"
+        nopat = operating_income * (1.0 - TAX_RATE)
+    except Exception:
+        nopat = 0.0
+    try:
+        invested_capital_df = client.get_concept(concept='netcashi')
+        ### Calculate Invested Capital 1-year average
+        start = invested_capital_df['val'].iloc[-4]
+        end = invested_capital_df['val'].iloc[-1]
+        avg_invested_capital = (start + end) / 2
+    except Exception:
+        avg_invested_capital = 0.0
+
+    if avg_invested_capital <= 0.0:
+        return {
+            'ticker': ticker,
+            'roic': 999999.0
+        }
+
+    roic = nopat / avg_invested_capital
+
+    return {
+        'ticker': ticker,
+        'roic': roic
+    }
 
 def ratio_net_profit_margin():
     return
