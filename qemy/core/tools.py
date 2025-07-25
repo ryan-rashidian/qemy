0175ce7f2beca43020f17d176769018c6c0c54dd
ryan-rashidian <ryanrashidian@gmail.com>
2025-07-25 13:23:37 -0700

    NEW Add get_concept_shaped function to /core/tools module to fill missing DataFrame fields for merging purposes, etc.

--- qemy/core/tools.py
+++ qemy/core/tools.py
@@ -2,8 +2,40 @@
 
 import pandas as pd
 
+from qemy.data import EDGARClient
 from qemy.exceptions import DataError
 
+
+def get_concept_shaped(
+    ticker: str,
+    concept: str,
+    quarters: int=10
+) -> pd.DataFrame:
+    """Get concept dataframe with missing values filled
+
+    Ensures consistent data shape for returns,
+    - if given the same 'quarters' arg.
+
+    Args:
+        ticker (str): Company ticker symbol
+        concept (str): Concept tag/key
+        quarters (int): # of quarters to fetch
+
+    Returns:
+        pd.DataFrame = With missing fields filled in
+    """
+    client = EDGARClient(ticker)
+    try:
+        return client.get_concept(
+            concept=concept,
+            quarters=quarters
+        )
+    except Exception:
+        return pd.DataFrame({
+            'val': [0.0] * quarters,
+            'filed': [pd.NaT] * quarters,
+            'form': ['N/A'] * quarters
+        })
 
 def get_ttm(series: pd.Series) -> float:
     """Trailing twelve months."""
