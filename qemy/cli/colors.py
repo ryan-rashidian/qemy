5c1dfda40fe929b643ffe0095dcb94e037d3baef
ryan-rashidian <ryanrashidian@gmail.com>
2025-08-18 15:30:11 -0700

    NEW Add colors.py registry module in /cli/ for Rich library coloring

---
+++ qemy/cli/colors.py
@@ -0,0 +1,26 @@
+"""Color registery for Qemy CLI.
+
+List of standard colors, and string syntax available at:
+https://rich.readthedocs.io/en/stable/appendix/colors.html
+"""
+
+# Themes
+info = 'magenta'
+data = 'green'
+warning = 'bold red'
+
+# Dataframe
+df_title = 'bold magenta underline'
+df_border = 'magenta'
+row_style = ['dim', '']
+metric_col = 'plum1'
+metric_col_header = 'bold plum1'
+value_col = 'green'
+value_col_header = 'bold green'
+default_col = 'magenta'
+default_col_header = 'bold magenta'
+
+# Panels
+panel_text = 'bold magenta'
+panel_border = 'magenta'
+
