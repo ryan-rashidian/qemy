b18e924599a33ff6f71b8d1ecb0a32223e1640da
ryan-rashidian <ryanrashidian@gmail.com>
2025-08-18 15:49:48 -0700

    NEW Add fsync command to Qemy CLI that calls bulk_refresh

--- qemy/cli/cmd_register.py
+++ qemy/cli/cmd_register.py
@@ -1,12 +1,13 @@
 """Command registry for Qemy CLI."""
 
-from qemy.cli.commands.api_edgar import cmd_filing
+from qemy.cli.commands.api_edgar import cmd_filing, cmd_bulk_refresh
 from qemy.cli.commands.core import cmd_score
 from qemy.cli.info.helper import print_help
 
 cmd_reg = {
     'f': cmd_filing,
     's': cmd_score,
+    'fsync': cmd_bulk_refresh,
     'help': print_help,
     '?': print_help
 }
--- qemy/cli/commands/api_edgar.py
+++ qemy/cli/commands/api_edgar.py
@@ -1,10 +1,13 @@
 """API EDGAR module for Qemy CLI."""
 
-from qemy.data import EDGARClient
+from qemy.data import EDGARClient, bulk_refresh
 from qemy.cli.format import print_df
+
+cmd_bulk_refresh = bulk_refresh
 
 def cmd_filing(ticker: str):
     ticker = ticker.upper().strip()
     filing = EDGARClient(ticker).get_filing()
     print_df(df=filing, title=f'Latest SEC Filing for: {ticker}')
+
 
