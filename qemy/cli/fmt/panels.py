0bad576458246b43b20f2d98c6f707f8b90e35da
ryan-rashidian <ryanrashidian@gmail.com>
2025-08-30 21:56:08 -0700

    NEW Add description_panel function to format terminal output for descriptions and refactor welcome_panel in /fmt/panels module

--- qemy/cli/fmt/panels.py
+++ qemy/cli/fmt/panels.py
@@ -1,26 +1,31 @@
-"""Panel renderable module for Qemy CLI.
-
-Using Rich library.
-"""
+"""Panel renderable module for Qemy CLI."""
 
 from rich.panel import Panel
 from rich.text import Text
 
 from qemy.cli.fmt import colors
-from qemy.cli.fmt.format import console
+from qemy.cli.fmt.format import console, format_text
 
-
-def style_panel_text(message: str):
-    text = Text(message, justify='center')
-    text.stylize(colors.panel_text)
-    return text
 
 def welcome_panel():
+    welcome_txt = Text('Qemy CLI 0.1.1', justify='center')
+    welcome_txt.stylize(colors.panel_text)
     panel = Panel(
-        style_panel_text('Qemy CLI 0.1.1'),
+        welcome_txt,
         title='Welcome.',
         subtitle="Type 'help' or '?' for info",
         border_style=colors.panel_border
     )
     console.print(panel)
+
+def description_panel(description: str, label: str) -> Panel:
+    description_fmt = format_text(description, theme='info')
+    label_fmt = format_text(label, theme='title', pos='center')
+    panel = Panel(
+        description_fmt,
+        title = label_fmt,
+        border_style = colors.panel_border,
+        padding = 3,
+    )
+    return panel
 
