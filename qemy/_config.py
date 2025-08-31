3e06f2d6bfeb6052fc9d9e2064a232d9dd2b4919
ryan-rashidian <ryanrashidian@gmail.com>
2025-08-31 15:29:54 -0700

    ENH Add INFO_DIR path and use PROJECT_ROOT path for fetching dotenv_path in _config module

--- qemy/_config.py
+++ qemy/_config.py
@@ -1,3 +1,8 @@
+"""Configuration file for Qemy.
+
+Central location for URLs, directory paths, and dotenv user credentials.
+"""
+
 import sys
 from pathlib import Path
 
@@ -5,9 +10,36 @@ from dotenv import load_dotenv
 
 from qemy.utils.env_tools import get_env_str
 
-# === KEY ===
+# === URL ===
+FRED_URL = 'https://api.stlouisfed.org/fred/series/observations'
 
-dotenv_path = Path(__file__).resolve().parents[1] / '.env'
+TIINGO_URL = 'https://api.tiingo.com/tiingo/daily/'
+TIINGO_IEX_URL = 'https://api.tiingo.com/iex/'
+
+EDGAR_CIK_URL = 'https://www.sec.gov/files/company_tickers.json'
+EDGAR_FACTS_URL = 'https://data.sec.gov/api/xbrl/companyfacts/CIK'
+EDGAR_ZIP_URL = 'https://www.sec.gov/Archives/edgar/daily-index/xbrl/companyfacts.zip'
+
+# === PATH ===
+# Check if executable before determining root path
+if getattr(sys, 'frozen', False):
+    PROJECT_ROOT = Path(sys.executable).resolve().parent
+else:
+    PROJECT_ROOT = Path(__file__).resolve().parents[1]
+
+INFO_DIR = PROJECT_ROOT / 'qemy' / 'cli' / 'info'
+
+EXPORT_CHART_DIR = PROJECT_ROOT / 'exports' / 'charts'
+EXPORT_CHART_DIR.mkdir(parents=True, exist_ok=True)
+
+EXPORT_TABLE_DIR = PROJECT_ROOT / 'exports' / 'tables'
+EXPORT_TABLE_DIR.mkdir(parents=True, exist_ok=True)
+
+BULK_DIR = PROJECT_ROOT / 'bulk_data'
+BULK_DIR.mkdir(parents=True, exist_ok=True)
+
+# === KEY ===
+dotenv_path = PROJECT_ROOT / '.env'
 if dotenv_path.exists():
     load_dotenv(dotenv_path=dotenv_path)
 
@@ -20,32 +52,4 @@ def tiingo_api_key() -> str:
 
 def edgar_user_agent() -> str:
     return get_env_str('EDGAR_USER_AGENT')
-
-# === URL ===
-
-FRED_URL = "https://api.stlouisfed.org/fred/series/observations"
-
-TIINGO_URL = "https://api.tiingo.com/tiingo/daily/"
-TIINGO_IEX_URL = "https://api.tiingo.com/iex/"
-
-EDGAR_CIK_URL = "https://www.sec.gov/files/company_tickers.json"
-EDGAR_FACTS_URL = "https://data.sec.gov/api/xbrl/companyfacts/CIK"
-EDGAR_ZIP_URL = "https://www.sec.gov/Archives/edgar/daily-index/xbrl/companyfacts.zip"
-
-# === PATH ===
-
-# Check if executable before determining root path
-if getattr(sys, 'frozen', False):
-    PROJECT_ROOT = Path(sys.executable).resolve().parent
-else:
-    PROJECT_ROOT = Path(__file__).resolve().parents[1]
-
-EXPORT_CHART_DIR = PROJECT_ROOT / "exports" / "charts"
-EXPORT_CHART_DIR.mkdir(parents=True, exist_ok=True)
-
-EXPORT_TABLE_DIR = PROJECT_ROOT / "exports" / "tables"
-EXPORT_TABLE_DIR.mkdir(parents=True, exist_ok=True)
-
-BULK_DIR = PROJECT_ROOT / "bulk_data"
-BULK_DIR.mkdir(parents=True, exist_ok=True)
 
