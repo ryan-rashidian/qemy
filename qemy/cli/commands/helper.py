4b61ba97e26e8d7e7dca6c476ea1a8429a9edd94
ryan-rashidian <ryanrashidian@gmail.com>
2025-08-31 21:30:44 -0700

    ENH Update print_help function for use with new help_register to display help information for individual CLI commands

--- qemy/cli/commands/helper.py
+++ qemy/cli/commands/helper.py
@@ -7,12 +7,22 @@ from rich.markdown import Markdown
 
 from qemy._config import INFO_DIR
 from qemy.cli.fmt.format import console
+from qemy.cli.fmt.panels import help_panel
+from qemy.cli.help.help_register import help_reg
 
 
-def print_help():
-    help_path = INFO_DIR / "help.md"
-    md_text = help_path.read_text()
-    console.print(Markdown(md_text), style='info')
+def print_help(cmd: str='help'):
+    if cmd == 'help':
+        help_path = INFO_DIR / "help.md"
+        md_text = help_path.read_text()
+        console.print(Markdown(md_text), style='info')
+    else:
+        help_txt = help_reg.get(cmd)
+        if help_txt:
+            help_pnl = help_panel(txt=help_txt, title=f'{cmd} - command')
+            console.print(help_pnl)
+        else:
+            console.print(f'{cmd} not found', style='warning')
 
 def print_info():
     info_path = INFO_DIR / "info.md"
