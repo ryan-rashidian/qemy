610941559076be6e03b0fdc071356e7ad46db638
ryan-rashidian <ryanrashidian@gmail.com>
2025-09-10 12:21:08 -0700

    ENH Add type hints and more detailed docstring in config.paths module

--- src/qemy/config/paths.py
+++ src/qemy/config/paths.py
@@ -1,13 +1,16 @@
-"""Filesystem paths used by Qemy."""
+"""Filesystem paths used by Qemy.
+
+Contains constants with pathlib.Path objects pointing to directories.
+"""
 
 import sys
 from pathlib import Path
 
 if getattr(sys, 'frozen', False):
-    PROJECT_ROOT = Path(sys.executable).resolve().parent
+    PROJECT_ROOT: Path = Path(sys.executable).resolve().parent
 else:
-    PROJECT_ROOT = Path(__file__).resolve().parents[2]
+    PROJECT_ROOT: Path = Path(__file__).resolve().parents[2]
 
-DOTENV_PATH = PROJECT_ROOT / '.env'
-BULK_DATA_DIR = PROJECT_ROOT / 'bulk_data'
+DOTENV_PATH: Path = PROJECT_ROOT / '.env'
+BULK_DATA_DIR: Path = PROJECT_ROOT / 'bulk_data'
 
