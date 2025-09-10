32c64c2ce5e2b544e6b4d3544b315a1d01e769b3
ryan-rashidian <ryanrashidian@gmail.com>
2025-09-09 22:41:14 -0700

    NEW Add write_credential and require_credential functions as wrappers for dotenv within Qemy

--- src/qemy/config/credentials.py
+++ src/qemy/config/credentials.py
@@ -1,11 +1,26 @@
 """API credentials and authentication for Qemy."""
 
-def write_edgar_credentials(credentials: str):
-    pass
+import os
+from dotenv import load_dotenv, set_key
 
-def write_fred_credentials(credentials: str):
-    pass
+from qemy.exceptions import MissingCredentialError
+from qemy.config.paths import DOTENV_PATH
 
-def write_tiingo_credentials(credentials: str):
-    pass
+if not DOTENV_PATH.exists():
+    DOTENV_PATH.touch()
+
+load_dotenv(dotenv_path=DOTENV_PATH)
+
+def write_credential(env_var: str, value: str) -> None:
+    """Write user-credentials to the .env file."""
+    set_key(DOTENV_PATH, env_var, value)
+
+def require_credential(service: str, env_var: str) -> str:
+    """Require user-credentials for access."""
+    value = os.getenv(env_var)
+    if not value:
+        raise MissingCredentialError(
+            f'{service} API credentials are not configured.'
+        )
+    return value
 
