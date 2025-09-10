d40fb57316553a63bd8c186fb68195df95c7eaaa
ryan-rashidian <ryanrashidian@gmail.com>
2025-09-10 01:04:13 -0700

    NEW Add custom InvalidSyntaxError exception for when invalid syntax is used in a string argument

--- src/qemy/exceptions.py
+++ src/qemy/exceptions.py
@@ -4,6 +4,10 @@ class ClientRequestError(Exception):
     """Raised when a client request fails or encounters an error."""
     pass
 
+class InvalidSyntaxError(Exception):
+    """Raised when invalid syntax is used in a string."""
+    pass
+
 class MissingCredentialError(Exception):
     """Raised when a required API credential is missing."""
     pass
