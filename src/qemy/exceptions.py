86a4c4a2995f8c109a9f9d4504d585c0aa40aef1
ryan-rashidian <ryanrashidian@gmail.com>
2025-09-10 00:29:26 -0700

    NEW Add custom ClientRequestError exception for when requests fail

--- src/qemy/exceptions.py
+++ src/qemy/exceptions.py
@@ -1,5 +1,9 @@
 """Custom exceptions for Qemy."""
 
+class ClientRequestError(Exception):
+    """Raised when a client request fails or encounters an error."""
+    pass
+
 class MissingCredentialError(Exception):
     """Raised when a required API credential is missing."""
     pass
