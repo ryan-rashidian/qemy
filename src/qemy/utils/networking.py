28a04a978f14ba22a985f61f743c00b741b715ae
ryan-rashidian <ryanrashidian@gmail.com>
2025-09-10 00:32:22 -0700

    NEW Add utils.networking module with make_request wrapper function for requests

---
+++ src/qemy/utils/networking.py
@@ -0,0 +1,43 @@
+"""Networking utilities for Qemy."""
+
+import requests
+
+from qemy.exceptions import ClientRequestError
+
+def make_request(
+    url: str,
+    headers: dict[str, str] | None = None,
+    params: dict[str, str] | None = None
+) -> dict:
+    """Make a request wrapped with error handling.
+
+    Args:
+        url (str): request URL
+        headers (str): request headers
+        params (str): request parameters
+
+    Returns:
+        dict: dictionary of requested data
+
+    Raises:
+        ClientRequestError: if request fails
+
+    Note:
+        `requests.get` accepts None for `headers` and `params`,
+        which means they are ignored if not provided.
+    """
+    try:
+        response = requests.get(url=url, headers=headers, params=params)
+        response.raise_for_status()
+        return response.json()
+
+    except requests.exceptions.HTTPError as e:
+        raise ClientRequestError(f'HTTP Error\n{e}')
+    except requests.exceptions.RequestException as e:
+        raise ClientRequestError(f'Request Error\n{e}')
+    except Exception as e:
+        raise ClientRequestError(f'Unexpected request Error\n{e}')
+
+def download_data():
+    pass
+
