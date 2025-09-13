af4b612d7b59b58b9649604056289185bef87f30
ryan-rashidian <ryanrashidian@gmail.com>
2025-09-13 01:36:50 -0700

    NEW Begin writing ConceptParser class in edgar.parser module

--- src/qemy/clients/edgar/parser.py
+++ src/qemy/clients/edgar/parser.py
@@ -1,1 +1,15 @@
-""""""
+"""
+Concept parsing and mapping.
+
+Assemble Concept dataclass containers from raw companyfacts data.
+"""
+
+from qemy.exceptions import ClientParsingError
+from qemy.clients.edgar.schemas import CompanyFacts, Concept
+
+class ConceptParser:
+    """Concept parser for EDGAR client."""
+
+    def __init__(self, companyfacts: CompanyFacts):
+        self.companyfacts = companyfacts
+
