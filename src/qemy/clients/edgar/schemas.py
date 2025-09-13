f340a3a5b79c7621ffe36ca5f0e88084a2efd50c
ryan-rashidian <ryanrashidian@gmail.com>
2025-09-13 00:42:35 -0700

    ENH Use dataclasses.fields function to set 'concepts' field default value in CompanyFacts and add repr methods to all dataclasses in edgar.schemas module

--- src/qemy/clients/edgar/schemas.py
+++ src/qemy/clients/edgar/schemas.py
@@ -1,23 +1,38 @@
-"""Dataclasses representing intermediate structures for SEC filing data."""
+"""
+Containers representing intermediate structures for SEC filing data.
 
-from dataclasses import dataclass
+Using dataclasses for companyfacts and parsed concepts.
+"""
 
-import pandas as pd
+from dataclasses import dataclass, field
+
 
 @dataclass
 class Concept:
+    """Container for parsed filings and metadata."""
     tag: str
     label: str
     description: str
     unit: str
     filings: list[dict]
 
-    def to_dataframe(self): # Placeholder/Reminder
-        return pd.DataFrame(self.filings)
+    def __repr__(self) -> str:
+        return (
+            f"Concept(tag='{self.tag}', label='{self.label}', "
+            f"unit='{self.unit}', filings=#{len(self.filings)} filed)"
+        )
 
 @dataclass
 class CompanyFacts:
+    """Container for parsed and sorted companyfacts data."""
     ticker: str
     entity_name: str
-    concepts: dict[str, Concept] | None = None
+    concepts: dict[str, Concept] = field(default_factory=dict)
+
+    def __repr__(self) -> str:
+        return (
+            f"CompanyFacts(ticker='{self.ticker}', "
+            f"entity_name='{self.entity_name}', "
+            f'concepts=#{len(self.concepts)} concepts)'
+        )
 
