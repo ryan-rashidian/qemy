202f243811f581e8152fcec68caecb3dd234c480
ryan-rashidian <ryanrashidian@gmail.com>
2025-09-14 19:19:46 -0700

    NEW Add minimum logging configuration with wrapped functions in config.logger module

--- src/qemy/config/logger.py
+++ src/qemy/config/logger.py
@@ -1,1 +1,16 @@
 """Logging configuration for Qemy."""
+
+import logging
+
+logging.basicConfig(
+    level = logging.INFO
+)
+
+def get_logger(name: str) -> logging.Logger:
+    """Return a logger using the global config."""
+    return logging.getLogger(name)
+
+def set_log_level(level: int) -> None:
+    """Set global logging level."""
+    logging.basicConfig(level=level, force=True)
+
