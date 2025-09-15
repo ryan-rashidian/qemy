dd35d7ca8e9ffa3051d513d34664153b109f3fe1
ryan-rashidian <ryanrashidian@gmail.com>
2025-09-15 15:59:39 -0700

    NEW Add cli.format.colors module for defining Rich colors and themes

---
+++ src/qemy/cli/format/colors.py
@@ -0,0 +1,28 @@
+"""Color registery for Qemy CLI.
+
+List of standard colors, and string syntax available at:
+https://rich.readthedocs.io/en/stable/appendix/colors.html
+"""
+
+# Themes
+info = 'magenta'
+data = 'green'
+warning = 'bold red'
+title = 'bold magenta underline'
+
+# DataFrame
+df_title = 'bold magenta underline'
+df_border = 'magenta'
+row_style = ['dim', '']
+value_col = 'green'
+value_col_header = 'bold green'
+default_col = 'magenta'
+default_col_header = 'bold magenta'
+
+# Panels
+panel_text = 'bold magenta'
+panel_border = 'magenta'
+
+# Misc.
+load_spinner = 'magenta'
+
