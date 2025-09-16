9c56a0fde4c59bbe766b01203f3ee8b656ea2d5f
ryan-rashidian <ryanrashidian@gmail.com>
2025-09-16 01:48:43 -0700

    ENH Fill cmd_registry with new CLI commands in cli.cmd_register module

--- src/qemy/cli/cmd_register.py
+++ src/qemy/cli/cmd_register.py
@@ -1,5 +1,13 @@
 """Command registry for Qemy CLI."""
 
+from qemy.cli.commands import (
+    cmd_calc,
+    cmd_clear
+)
+
 cmd_registry = {
+    'calc': cmd_calc,
+    'clear': cmd_clear,
+    'cls': cmd_clear
 }
 
