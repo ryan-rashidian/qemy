e2d3791c9924193e3ab6c0c0025d4d0e2394031c
ryan-rashidian <ryanrashidian@gmail.com>
2025-09-18 02:42:03 -0700

    ENH Import cmd_f and cmd_fc to commands package level and add to the Qemy CLI command register

--- src/qemy/cli/cmd_register.py
+++ src/qemy/cli/cmd_register.py
@@ -1,11 +1,19 @@
 """Command registry for Qemy CLI."""
 
-from qemy.cli.commands import cmd_calc, cmd_clear, cmd_fsync
+from qemy.cli.commands import (
+    cmd_calc,
+    cmd_clear,
+    cmd_f,
+    cmd_fc,
+    cmd_fsync
+)
 
 cmd_registry = {
     'calc': cmd_calc,
     'clear': cmd_clear,
     'cls': cmd_clear,
+    'f': cmd_f,
+    'fc': cmd_fc,
     'fsync': cmd_fsync
 }
 
--- src/qemy/cli/commands/__init__.py
+++ src/qemy/cli/commands/__init__.py
@@ -1,9 +1,11 @@
 from qemy.cli.commands.auxiliary import cmd_calc, cmd_clear
-from qemy.cli.commands.edgar_cli import cmd_fsync
+from qemy.cli.commands.edgar_cli import cmd_f, cmd_fc, cmd_fsync
 
 __all__ = [
     'cmd_calc',
     'cmd_clear',
+    'cmd_f',
+    'cmd_fc',
     'cmd_fsync'
 ]
 
