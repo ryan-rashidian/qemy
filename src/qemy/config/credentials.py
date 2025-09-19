be54147b5c9e805e2f1ce39ef627b212c3458fac
ryan-rashidian <ryanrashidian@gmail.com>
2025-09-18 20:33:39 -0700

    NEW Add remove_credential function to config.credentials module for removing .env variables

--- src/qemy/config/credentials.py
+++ src/qemy/config/credentials.py
@@ -1,8 +1,9 @@
 """API credentials and authentication for Qemy."""
 
 import os
+from typing import Optional, Tuple
 
-from dotenv import load_dotenv, set_key
+from dotenv import load_dotenv, set_key, unset_key
 
 from qemy.config.paths import DOTENV_PATH
 from qemy.exceptions import MissingCredentialError
@@ -15,6 +16,10 @@ load_dotenv(dotenv_path=DOTENV_PATH)
 def write_credential(env_var: str, value: str) -> None:
     """Write user-credentials to the .env file."""
     set_key(DOTENV_PATH, env_var, value)
+
+def remove_credential(env_var: str) -> Tuple[Optional[bool], str]:
+    """Remove user-credentials from the .env file."""
+    return unset_key(DOTENV_PATH, env_var)
 
 def require_credential(service: str, env_var: str) -> str:
     """Require user-credentials for access."""
