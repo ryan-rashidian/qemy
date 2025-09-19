37ff4ac79e32283a22e7fbfed3c6c4e8f641d598
ryan-rashidian <ryanrashidian@gmail.com>
2025-09-18 23:12:04 -0700

    NEW Add format.panels module with welcome_panel and info_panel functions for displaying Rich Panels in Qemy CLI

---
+++ src/qemy/cli/format/panels.py
@@ -0,0 +1,43 @@
+"""Rich panel renders for Qemy CLI."""
+
+from rich.panel import Panel
+
+from qemy.cli.format import colors
+from qemy.cli.format.fmt import console, FormatText
+
+
+def welcome_panel() -> None:
+    """Qemy CLI welcome panel."""
+    welcome_text = FormatText(
+        'Qemy CLI 0.2.0'
+    ).justify('center').style(colors.panel_text).get_text()
+
+    panel = Panel(
+        welcome_text,
+        title = 'Welcome.',
+        subtitle = "Type 'help' or '?' for info",
+        border_style = colors.panel_border,
+        padding = 3
+    )
+
+    console.print(panel)
+
+def info_panel(txt: str, title: str) -> None:
+    """Qemy CLI info panel.
+
+    Args:
+        txt (str): Body of the panel
+        title (str): Title of the panel
+    """
+    txt_fmt = FormatText(txt).style('info').get_text()
+    title_fmt = FormatText(title).justify('center').style('title').get_text()
+
+    panel = Panel(
+        txt_fmt,
+        title = title_fmt,
+        border_style = colors.panel_border,
+        padding = 3
+    )
+
+    console.print(panel)
+
