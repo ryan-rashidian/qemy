aea2037494c25015e1e82b093ccbf02ca0840260
ryan-rashidian <ryanrashidian@gmail.com>
2025-09-19 00:58:57 -0700

    NEW Add metrics.cashflow_statement module with FreeCashFlow class for calculating free cash flow

---
+++ src/qemy/analytics/metrics/cashflow_statement.py
@@ -0,0 +1,25 @@
+"""Cash-Flow Statment metrics."""
+
+import pandas as pd
+
+from qemy.analytics.base import EDGARMetrics
+
+
+class FreeCashFlow(EDGARMetrics):
+    """Calculate Free Cash Flow."""
+
+    def __init__(self):
+        """Initialize combined concept DataFrame."""
+        df_ocf: pd.DataFrame = self.get_concept_df('ocf')
+        df_capex: pd.DataFrame = self.get_concept_df('capex')
+        self.df_combined = self.merge_concept_dfs(*[df_ocf, df_capex])
+
+    def get_fcf(self) -> pd.DataFrame:
+        """Get Free Cash Flow DataFrame."""
+        self.df_combined['val'] = (
+            self.df_combined['val_ocf'] - self.df_combined['val_capex']
+        )
+        df_fcf = self.df_combined.drop(['val_ocf', 'val_capex'], axis=1)
+
+        return df_fcf.sort_values('filed')
+
