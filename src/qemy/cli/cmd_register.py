eeef67374c20b95bd46c7f23b8f8b6d43dc93d47
ryan-rashidian <ryanrashidian@gmail.com>
2025-09-18 20:41:48 -0700

    ENH Import cmd_env and cmd_rmenv to package level and add to cmd_register for access withing the CLI

--- src/qemy/cli/cmd_register.py
+++ src/qemy/cli/cmd_register.py
@@ -1,13 +1,23 @@
 """Command registry for Qemy CLI."""
 
-from qemy.cli.commands import cmd_calc, cmd_clear, cmd_f, cmd_fc, cmd_fsync
+from qemy.cli.commands import (
+    cmd_calc,
+    cmd_clear,
+    cmd_env,
+    cmd_f,
+    cmd_fc,
+    cmd_fsync,
+    cmd_rmenv,
+)
 
 cmd_registry = {
     'calc': cmd_calc,
     'clear': cmd_clear,
     'cls': cmd_clear,
+    'env': cmd_env,
     'f': cmd_f,
     'fc': cmd_fc,
-    'fsync': cmd_fsync
+    'fsync': cmd_fsync,
+    'rmenv': cmd_rmenv
 }
 
--- src/qemy/cli/commands/__init__.py
+++ src/qemy/cli/commands/__init__.py
@@ -1,11 +1,13 @@
-from qemy.cli.commands.auxiliary import cmd_calc, cmd_clear
+from qemy.cli.commands.auxiliary import cmd_calc, cmd_clear, cmd_env, cmd_rmenv
 from qemy.cli.commands.edgar_cli import cmd_f, cmd_fc, cmd_fsync
 
 __all__ = [
     'cmd_calc',
     'cmd_clear',
+    'cmd_env',
     'cmd_f',
     'cmd_fc',
-    'cmd_fsync'
+    'cmd_fsync',
+    'cmd_rmenv'
 ]
 
