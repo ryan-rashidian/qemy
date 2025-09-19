0a50ab3fdc113ebd9d3027da5e851b330401bdeb
ryan-rashidian <ryanrashidian@gmail.com>
2025-09-19 15:42:54 -0700

    ENH Add EDGARMetrics.get_concept_df_safe method as a fail safe variant for when concept data is not critical to a metric calculations final results or is a cumulative component of a concept that may or may not exist

--- src/qemy/analytics/base.py
+++ src/qemy/analytics/base.py
@@ -5,10 +5,14 @@ from functools import reduce
 import pandas as pd
 
 from qemy.clients import EDGARClient
+from qemy.exceptions import ClientParsingError
 
 
 class EDGARMetrics:
     """Base class for EDGAR Metrics."""
+    ON_COLUMNS = [
+        'accn', 'fp', 'fy', 'filed', 'form', 'start', 'end', 'frame'
+    ]
 
     def __init__(self, ticker: str):
         """Initialize Client for EDGAR Metrics."""
@@ -16,7 +20,7 @@ class EDGARMetrics:
         self.client = EDGARClient(self.ticker)
 
     def get_concept_df(self, concept: str) -> pd.DataFrame:
-        """Return pandas DataFrame of given concept."""
+        """Return pandas DataFrame of a concept."""
         concept_fmt = concept.strip().lower()
         df_concept = self.client.get_concept(
             concept = concept_fmt
@@ -30,16 +34,36 @@ class EDGARMetrics:
 
         return df_concept
 
+    def get_concept_df_safe(self, concept: str) -> pd.DataFrame:
+        """Return pandas DataFrame of a concept.
+
+        Return placeholder DataFrame if parsing error occurs.
+        """
+        concept_fmt = concept.strip().lower()
+
+        try:
+            df_concept = self.client.get_concept(
+                concept = concept_fmt
+            ).companyfacts.concepts[concept].to_dataframe()
+        except ClientParsingError:
+            columns = pd.Index(self.ON_COLUMNS + ['val'])
+            df_concept = pd.DataFrame(columns=columns)
+
+        if 'val' in df_concept.columns:
+            df_concept.rename(
+                columns = {'val': f'val_{concept_fmt}'},
+                inplace = True
+            )
+
+        return df_concept
+
     def merge_concept_dfs(self, *dataframes: pd.DataFrame) -> pd.DataFrame:
         """Merge concept dataframes."""
-        on_columns = [
-            'accn', 'fp', 'fy', 'filed', 'form', 'start', 'end', 'frame'
-        ]
         df_merged = reduce(
             lambda df_left, df_right: pd.merge(
                 df_left,
                 df_right,
-                on = on_columns,
+                on = self.ON_COLUMNS,
                 how = 'outer'
             ),
             dataframes
