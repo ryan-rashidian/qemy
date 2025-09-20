87a693f51d8e5cf38ffa988bbd89096460622650
ryan-rashidian <ryanrashidian@gmail.com>
2025-09-20 03:03:50 -0700

    NEW Add panels.title_panel function for printing Rich Panel formatted for titles

--- src/qemy/cli/format/panels.py
+++ src/qemy/cli/format/panels.py
@@ -6,22 +6,6 @@ from qemy.cli.format import colors
 from qemy.cli.format.fmt import FormatText, console
 
 
-def welcome_panel() -> None:
-    """Qemy CLI welcome panel."""
-    welcome_text = FormatText(
-        'Qemy CLI 0.2.0'
-    ).justify('center').style(colors.panel_text).get_text()
-
-    panel = Panel(
-        welcome_text,
-        title = 'Welcome.',
-        subtitle = "Type 'help' or '?' for info",
-        border_style = colors.panel_border,
-        padding = 3
-    )
-
-    console.print(panel)
-
 def info_panel(txt: str, title: str) -> None:
     """Qemy CLI info panel.
 
@@ -35,6 +19,41 @@ def info_panel(txt: str, title: str) -> None:
     panel = Panel(
         txt_fmt,
         title = title_fmt,
+        border_style = colors.panel_border,
+        padding = 3
+    )
+
+    console.print(panel)
+
+def title_panel(txt: str, title: str) -> None:
+    """Qemy CLI title panel.
+
+    Args:
+        txt (str): Body of the panel
+        title (str): Title of the panel
+    """
+    txt_fmt = FormatText(txt).justify('center').style('title').get_text()
+    title_fmt = FormatText(title).justify('center').style('title').get_text()
+
+    panel = Panel(
+        txt_fmt,
+        title = title_fmt,
+        border_style = colors.panel_border,
+        padding = 3
+    )
+
+    console.print(panel)
+
+def welcome_panel() -> None:
+    """Qemy CLI welcome panel."""
+    welcome_text = FormatText(
+        'Qemy CLI 0.2.0'
+    ).justify('center').style(colors.panel_text).get_text()
+
+    panel = Panel(
+        welcome_text,
+        title = 'Welcome.',
+        subtitle = "Type 'help' or '?' for info",
         border_style = colors.panel_border,
         padding = 3
     )
