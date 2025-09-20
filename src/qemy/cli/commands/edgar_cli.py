961e3ec018f73ae3ace00a2280b97e8167cfc922
ryan-rashidian <ryanrashidian@gmail.com>
2025-09-20 03:05:28 -0700

    ENH Add title_panel to edgar_cli.cmd_f command function and clean module

--- src/qemy/cli/commands/edgar_cli.py
+++ src/qemy/cli/commands/edgar_cli.py
@@ -1,11 +1,10 @@
 """Commands for EDGARClient in Qemy CLI."""
 
-
 from rich.progress import BarColumn, Progress, SpinnerColumn, TextColumn
 
 from qemy.cli.format import colors
 from qemy.cli.format.fmt import FormatDF, FormatText
-from qemy.cli.format.panels import info_panel
+from qemy.cli.format.panels import info_panel, title_panel
 from qemy.cli.menus import confirm_menu
 from qemy.clients import EDGARClient
 from qemy.clients.edgar import (
@@ -21,9 +20,17 @@ from qemy.exceptions import ClientParsingError
 def cmd_f(ticker: str) -> None:
     """Print a summary of the latest filing for a given ticker."""
     client = EDGARClient(ticker)
+
+    description = 'Latest Filings Summary'
+    company = client.companyfacts.entity_name
+    ticker_fmt = client.companyfacts.ticker
+    title = f'{company} ({ticker_fmt})'
+
     balance_sheet_df = client.get_balance_sheet_df()
     cashflow_statement_df = client.get_cashflow_statement_df()
     income_statement_df = client.get_income_statement_df()
+
+    title_panel(txt=description, title=title)
     FormatDF(balance_sheet_df, 'Balance Sheet').print()
     FormatDF(cashflow_statement_df, 'Cash Flow Statement').print()
     FormatDF(income_statement_df, 'Income Statement').print()
@@ -43,10 +50,11 @@ def cmd_fc(ticker: str, concept: str, quarters: str='8') -> None:
         company = client.companyfacts.entity_name
         ticker_fmt = client.companyfacts.ticker
         title = f'{company} ({ticker_fmt}): {label} ({unit})'
-        info_panel(txt=description, title=title)
 
         concept_df = concept_data.to_dataframe().tail(quarters_int)
         concept_df.drop(columns=['fy', 'frame', 'end', 'accn'], inplace=True)
+
+        info_panel(txt=description, title=title)
         FormatDF(concept_df, f'{concept}').print()
 
     except ClientParsingError:
