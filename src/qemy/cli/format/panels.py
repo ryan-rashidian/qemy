0df24653b5f7600a23b99b221dd783cbf99048f3
ryan-rashidian <ryanrashidian@gmail.com>
2025-09-22 22:54:42 -0700

    NEW Add panels.markdown_panel function wrapping Rich Markdown styled text and printing inside of a panel

--- src/qemy/cli/format/panels.py
+++ src/qemy/cli/format/panels.py
@@ -1,18 +1,14 @@
 """Rich panel renders for Qemy CLI."""
 
 from rich.panel import Panel
+from rich.markdown import Markdown
 
 from qemy.cli.format import colors
 from qemy.cli.format.fmt import FormatText, console
 
 
 def info_panel(txt: str, title: str) -> None:
-    """Qemy CLI info panel.
-
-    Args:
-        txt (str): Body of the panel
-        title (str): Title of the panel
-    """
+    """Qemy CLI info panel."""
     txt_fmt = FormatText(txt).style('info').get_text()
     title_fmt = FormatText(title).justify('center').style('title').get_text()
 
@@ -26,12 +22,7 @@ def info_panel(txt: str, title: str) -> None:
     console.print(panel)
 
 def title_panel(txt: str, title: str) -> None:
-    """Qemy CLI title panel.
-
-    Args:
-        txt (str): Body of the panel
-        title (str): Title of the panel
-    """
+    """Qemy CLI title panel."""
     txt_fmt = FormatText(txt).justify('center').style('title').get_text()
     title_fmt = FormatText(title).justify('center').style('title').get_text()
 
@@ -44,13 +35,21 @@ def title_panel(txt: str, title: str) -> None:
 
     console.print(panel)
 
-def result_panel(txt: str, title: str) -> None:
-    """Qemy CLI result panel.
+def markdown_panel(txt: str, title: str) -> None:
+    """Qemy CLI markdown panel."""
+    md_txt = Markdown(txt)
+    title_fmt = FormatText(title).justify('center').style('title').get_text()
+    panel = Panel(
+        md_txt,
+        title = title_fmt,
+        border_style = colors.panel_border,
+        padding = 1
+    )
 
-    Args:
-        txt (str): Body of the panel
-        title (str): Title of the panel
-    """
+    console.print(panel, style='info')
+
+def result_panel(txt: str, title: str) -> None:
+    """Qemy CLI result panel."""
     txt_fmt = FormatText(txt).justify('center').style('data').get_text()
     title_fmt = FormatText(title).justify('center').style('title').get_text()
 
