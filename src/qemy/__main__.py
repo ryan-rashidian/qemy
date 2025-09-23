92dd9ab0dcbee1d7ccefed63d59a89dfe5f48ba4
ryan-rashidian <ryanrashidian@gmail.com>
2025-09-23 13:49:49 -0700

    ENH Turn off logging messages by default for Qemy CLI

--- src/qemy/__main__.py
+++ src/qemy/__main__.py
@@ -1,8 +1,16 @@
+"""Qemy CLI entry point."""
+
+import logging
+
+from qemy.config.logger import set_log_level
 from qemy.cli.cli_main import QemyCLI
 
 
 def main() -> None:
     """Run Qemy CLI."""
+    # Logging off by default in CLI
+    set_log_level(logging.CRITICAL + 1)
+
     QemyCLI().run()
 
 if __name__ == '__main__':
