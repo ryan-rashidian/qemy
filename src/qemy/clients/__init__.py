b333293e0d6756fe08f9ab4f821c8e2d76955861
ryan-rashidian <ryanrashidian@gmail.com>
2025-09-23 17:08:19 -0700

    ENH Add FREDClient to package level import for organization

--- src/qemy/clients/__init__.py
+++ src/qemy/clients/__init__.py
@@ -1,8 +1,10 @@
 from qemy.clients.edgar.client import EDGARClient
+from qemy.clients.fred.client import FREDClient
 from qemy.clients.tiingo.client import TiingoClient
 
 __all__ = [
     'EDGARClient',
+    'FREDClient',
     'TiingoClient'
 ]
 
