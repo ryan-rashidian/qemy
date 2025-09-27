098f9709145c006772e8f6deed6635c5c2c7b139
ryan-rashidian <ryanrashidian@gmail.com>
2025-09-26 23:18:28 -0700

    ENH Command function cmd_f now contains a sub-loop for navigating filings in CLI, remove now redundant cmd_fc command function

--- src/qemy/cli/commands/__init__.py
+++ src/qemy/cli/commands/__init__.py
@@ -1,5 +1,5 @@
 from qemy.cli.commands.auxiliary import cmd_calc, cmd_clear, cmd_env, cmd_rmenv
-from qemy.cli.commands.edgar_cli import cmd_f, cmd_fc, cmd_fsync
+from qemy.cli.commands.edgar_cli import cmd_f, cmd_fsync
 from qemy.cli.commands.help import cmd_help
 from qemy.cli.commands.models_cli import cmd_m
 from qemy.cli.commands.scores_cli import cmd_s
@@ -9,7 +9,6 @@ __all__ = [
     'cmd_clear',
     'cmd_env',
     'cmd_f',
-    'cmd_fc',
     'cmd_fsync',
     'cmd_help',
     'cmd_m',
--- src/qemy/cli/commands/edgar_cli.py
+++ src/qemy/cli/commands/edgar_cli.py
@@ -18,62 +18,106 @@ from qemy.clients.edgar.schemas import Concept
 from qemy.exceptions import ClientParsingError
 
 
+def _f_concept(client: EDGARClient) -> None:
+    """CLI Filings: Concept search."""
+    ticker_fmt = client.companyfacts.ticker.upper()
+    info_description = (
+        'Enter a valid concept and and number of quarters to fetch\n'
+        "For a list of valid concepts, enter `help` or `?`\n\n"
+        f'Usage: ({ticker_fmt}) (CONCEPTS) >>> <CONCEPT> <#QUARTERS>'
+    )
+    info_title = 'Concept Search.'
+    info_panel(txt=info_description, title=info_title)
+    concept, quarters = input(f'({ticker_fmt}) (CONCEPTS) ').split()
+
+    try:
+        client.fetch_concept(concept)
+        concept_data = client.companyfacts.concepts.get(concept, Concept())
+    except ClientParsingError:
+        FormatText(f'{concept} not found.\n').style('warning').print()
+        return
+
+    description = concept_data.description
+    label = concept_data.label
+    unit = concept_data.unit
+    company = client.companyfacts.name
+    ticker_fmt = client.companyfacts.ticker
+    title = f'{company} ({ticker_fmt}): {label} ({unit})'
+
+    quarters_int = int(quarters)
+    concept_df = concept_data.to_dataframe().tail(quarters_int)
+    concept_df.drop(columns=['fy', 'frame', 'end', 'accn'], inplace=True)
+
+    info_panel(txt=description, title=title)
+    FormatDF(concept_df, f'{concept}').print()
+
+def _f_balance_sheet(client: EDGARClient) -> None:
+    """CLI Filings: Balance Sheet"""
+    description = 'Balance Sheet'
+    company_name = client.companyfacts.name
+    ticker_fmt = client.companyfacts.ticker
+    title = f'{company_name} ({ticker_fmt})'
+
+    balance_sheet_df = client.get_balance_sheet_df()
+
+    title_panel(txt=description, title=title)
+    FormatDF(balance_sheet_df, description).print()
+
+def _f_cashflow_statement(client: EDGARClient) -> None:
+    """CLI Filings: Cash Flow Statement"""
+    description = 'Cash Flow Statement'
+    company_name = client.companyfacts.name
+    ticker_fmt = client.companyfacts.ticker
+    title = f'{company_name} ({ticker_fmt})'
+
+    balance_sheet_df = client.get_cashflow_statement_df()
+
+    title_panel(txt=description, title=title)
+    FormatDF(balance_sheet_df, description).print()
+
+def _f_income_statement(client: EDGARClient) -> None:
+    """CLI Filings: Income Statement"""
+    description = 'Income Statement'
+    company_name = client.companyfacts.name
+    ticker_fmt = client.companyfacts.ticker
+    title = f'{company_name} ({ticker_fmt})'
+
+    balance_sheet_df = client.get_income_statement_df()
+
+    title_panel(txt=description, title=title)
+    FormatDF(balance_sheet_df, description).print()
+
 @help_text("""\nCategory: [EDGAR]
-Description: Fetch a summary of the latest SEC 10K/10Q filing.
+Description: Fetch SEC 10K/10Q filing data.
 
 Usage: >>> f <TICKER>
 """)
 def cmd_f(ticker: str) -> None:
-    """Print a summary of the latest filing for a given ticker."""
+    """CLI command for SEC filings."""
     client = EDGARClient(ticker)
 
-    description = 'Latest Filings Summary'
-    company = client.companyfacts.name
-    ticker_fmt = client.companyfacts.ticker
-    title = f'{company} ({ticker_fmt})'
+    menu_description = 'SEC 10K/10Q filings.'
+    company_name = client.companyfacts.name
+    ticker_fmt = client.companyfacts.ticker.upper()
+    menu_title = f'{company_name} ({ticker_fmt})'
+    choices = (
+        '1. Concepts\n'
+        '2. Balance Sheet\n'
+        '3. Cash Flow Statment\n'
+        '4. Income Statement\n'
+        '5. Back'
+    )
+    info_title = 'Options:'
 
-    balance_sheet_df = client.get_balance_sheet_df()
-    cashflow_statement_df = client.get_cashflow_statement_df()
-    income_statement_df = client.get_income_statement_df()
-
-    title_panel(txt=description, title=title)
-    FormatDF(balance_sheet_df, 'Balance Sheet').print()
-    FormatDF(cashflow_statement_df, 'Cash Flow Statement').print()
-    FormatDF(income_statement_df, 'Income Statement').print()
-
-@help_text("""\nCategory: [EDGAR]
-Description: Fetch historical SEC 10K/10Q filing data for a CONCEPT.
-CONCEPT: These are synonymous with a 'metric' or 'field'
-    - for e.g. 'assets', 'shares', 'debt' are all examples of a CONCEPT
-    - type `fconcepts` to get a list of possible concepts with descriptions
-
-Usage: >>> fc <TICKER> <CONCEPT> <QUARTERS>
-""")
-def cmd_fc(ticker: str, concept: str, quarters: str='8') -> None:
-    """Print historical filing data for given ticker, concept to terminal."""
-    quarters_int = int(quarters)
-    try:
-        client = EDGARClient(ticker).fetch_concept(concept)
-        concept_data = client.companyfacts.concepts.get(concept, Concept())
-        if not concept_data.filings:
-            FormatText(f'No data for: {concept}\n').style('warning').print()
-
-        description = concept_data.description
-        label = concept_data.label
-        unit = concept_data.unit
-        company = client.companyfacts.name
-        ticker_fmt = client.companyfacts.ticker
-        title = f'{company} ({ticker_fmt}): {label} ({unit})'
-
-        concept_df = concept_data.to_dataframe().tail(quarters_int)
-        concept_df.drop(columns=['fy', 'frame', 'end', 'accn'], inplace=True)
-
-        info_panel(txt=description, title=title)
-        FormatDF(concept_df, f'{concept}').print()
-
-    except ClientParsingError:
-        FormatText(f'{concept} not found.\n').style('warning').print()
-        return
+    title_panel(txt=menu_description, title=menu_title)
+    while True:
+        info_panel(txt=choices, title=info_title)
+        choice = input(f'({ticker_fmt}) ').strip()
+        if choice == '1': _f_concept(client)
+        elif choice == '2': _f_balance_sheet(client)
+        elif choice == '3': _f_cashflow_statement(client)
+        elif choice == '4': _f_income_statement(client)
+        elif choice == '5': break
 
 @help_text("""\nCategory: [EDGAR]
 Description: Download SEC bulk filing data for use within Qemy CLI.
